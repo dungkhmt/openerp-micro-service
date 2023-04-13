4d2ab0942886d84d31f18a898343e706f253b181
QuangHa0110 <quanghadang08@gmail.com>
2023-04-13 10:21:06 +0700

    add redux

--- code-editor-online/openerp-code-editor-client/src/index.js
+++ code-editor-online/openerp-code-editor-client/src/index.js
@@ -3,9 +3,15 @@ import ReactDOM from "react-dom/client";
 import App from "./App";
 import "./index.css";
 import reportWebVitals from "./reportWebVitals";
+import { Provider } from "react-redux";
+import { store } from "store/store";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<App />);
+root.render(
+  <Provider store={store}>
+    <App />
+  </Provider>
+);
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
