74eb0085f1c9f5bbe847c56b701ab4c1bed8a138
QuangHa0110 <quanghadang08@gmail.com>
2023-04-13 10:20:32 +0700

    update constants

---
+++ code-editor-online/openerp-code-editor-client/src/utils/constants.js
@@ -0,0 +1,24 @@
+export const SOCKET_EVENTS = {
+    CONNECTION: "connection",
+    DISCONNECT: "disconnected",
+    CONNECT_TO_EDITOR: "connect-to-editor",
+    SEND_CODE_CHANGES: "send-code-changes",
+    RECEIVE_CODE_CHANGES: "receive-code-changes",
+    JOINED: "joined",
+  };
+  
+  export const PROGRAMMING_LANGUAGES = {
+    CPP: {
+      label: "C++",
+      value: "CPP",
+    },
+    JAVA: {
+      label: "Java",
+      value: "JAVA",
+    },
+    PYTHON: {
+      label: "Python",
+      value: "PYTHON",
+    },
+  };
+  
