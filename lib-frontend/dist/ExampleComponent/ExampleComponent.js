2cd9d3992c784d57e0a98c6bf690dd21cc2ef9b1
NguyenDuy-Tien <tien.nd194857@sis.hust.edu.vn>
2023-04-18 22:15:23 +0700

    lib: update lib frontend

---
+++ lib-frontend/dist/ExampleComponent/ExampleComponent.js
@@ -0,0 +1,12 @@
+"use strict";
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+exports.ExampleComponent = void 0;
+var _react = _interopRequireDefault(require("react"));
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { "default": obj }; }
+var ExampleComponent = function ExampleComponent() {
+  return /*#__PURE__*/_react["default"].createElement("div", null, /*#__PURE__*/_react["default"].createElement("h1", null, "Hello World"));
+};
+exports.ExampleComponent = ExampleComponent;
---
+++ lib-frontend/dist/index.js
@@ -0,0 +1,49 @@
+"use strict";
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+var _ExampleComponent = require("./ExampleComponent");
+Object.keys(_ExampleComponent).forEach(function (key) {
+  if (key === "default" || key === "__esModule") return;
+  if (key in exports && exports[key] === _ExampleComponent[key]) return;
+  Object.defineProperty(exports, key, {
+    enumerable: true,
+    get: function get() {
+      return _ExampleComponent[key];
+    }
+  });
+});
+var _StandardTable = require("./StandardTable");
+Object.keys(_StandardTable).forEach(function (key) {
+  if (key === "default" || key === "__esModule") return;
+  if (key in exports && exports[key] === _StandardTable[key]) return;
+  Object.defineProperty(exports, key, {
+    enumerable: true,
+    get: function get() {
+      return _StandardTable[key];
+    }
+  });
+});
+var _HustModal = require("./HustModal");
+Object.keys(_HustModal).forEach(function (key) {
+  if (key === "default" || key === "__esModule") return;
+  if (key in exports && exports[key] === _HustModal[key]) return;
+  Object.defineProperty(exports, key, {
+    enumerable: true,
+    get: function get() {
+      return _HustModal[key];
+    }
+  });
+});
+var _HustCodeEditor = require("./HustCodeEditor");
+Object.keys(_HustCodeEditor).forEach(function (key) {
+  if (key === "default" || key === "__esModule") return;
+  if (key in exports && exports[key] === _HustCodeEditor[key]) return;
+  Object.defineProperty(exports, key, {
+    enumerable: true,
+    get: function get() {
+      return _HustCodeEditor[key];
+    }
+  });
+});
