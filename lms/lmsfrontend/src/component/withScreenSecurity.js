53db40562d21975c5c3a85d184ee015a3cc19ee3
NguyenDuy-Tien <tien.nd194857@sis.hust.edu.vn>
2023-04-23 02:20:39 +0700

    feat: update withScreenSecurity

--- lms/lmsfrontend/src/component/withScreenSecurity.js
+++ lms/lmsfrontend/src/component/withScreenSecurity.js
@@ -1,38 +1,65 @@
-import React, {useEffect, useState} from "react";
-import {useDispatch, useSelector} from "react-redux";
-import {authGet} from "../api";
-import Loading from "./common/Loading";
+import { LinearProgress } from "@mui/material";
+import { useEffect, useState } from "react";
+import {request} from "../api";
 import NotAuthorized from "./common/NotAuthorzied";
 
-function withScreenSecurity(SecurityComponet, screenName, viewError) {
+/**
+ * The function withScreenSecurity is a higher-order component that adds screen security to a component
+ * by checking if the user has authorization to view it.
+ * @param SecuredComponent - The component that needs to be secured with screen authorization.
+ * @param id - The `id` parameter is a string that represents the unique identifier of the screen or
+ * component that needs to be secured. It is used to make a request to the server to check if the user
+ * has the necessary authorization to view the screen.
+ * @param viewError - A boolean value indicating whether to display a "Not Authorized" error message
+ * when the user does not have permission to view the secured component.
+ * @returns The function `withScreenSecurity` is being returned.
+ */
+function withScreenSecurity(SecuredComponent, id, viewError) {
   return function ScreenSecurityComponent({ ...props }) {
-    const token = useSelector((state) => state.auth.token);
-    const dispatch = useDispatch();
-    const [isChecking, setIsChecking] = useState(true);
-    const [isAuthorized, setIsAuthorized] = useState(false);
-    console.log(SecurityComponet.name);
+    const [checking, setChecking] = useState(true);
+    const [screenAuthorization, setScreenAuthorization] = useState(new Set([]));
+
     useEffect(() => {
-      setIsChecking(true);
-      authGet(
-        dispatch,
-        token,
-        "/check-authority" + "?applicationId=" + screenName
-      ).then(
+      setChecking(true);
+
+      request(
+        "get",
+        `/entity-authorization/${id}`,
         (res) => {
-          console.log(res);
-          setIsChecking(false);
-          if (res.result === "INCLUDED") setIsAuthorized(true);
+          setChecking(false);
+          setScreenAuthorization(new Set(res.data));
         },
-        (error) => {
-          console.log("error");
-          setIsChecking(false);
+        {
+          onError: (e) => {
+            setChecking(false);
+            console.log(e);
+          },
         }
       );
     }, []);
-    if (isChecking) return <Loading />;
-    else if (isAuthorized) return <SecurityComponet {...props} />;
+
+    if (checking)
+      return (
+        <LinearProgress
+          style={{
+            position: "absolute",
+            top: 0,
+            left: -1, // when use 0, the progress bar be blurred
+            width: "100%",
+            zIndex: 1202,
+          }}
+        />
+      );
+    else if (screenAuthorization.has(`${id}.VIEW`))
+      return (
+        <SecuredComponent
+          {...props}
+          screenAuthorization={screenAuthorization}
+        />
+      );
     else if (viewError) return <NotAuthorized />;
     else return "";
   };
 }
+
 export default withScreenSecurity;
