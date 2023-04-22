c312132c40ac8f17001df05e4836ccdbd9afa9d2
NguyenDuy-Tien <tien.nd194857@sis.hust.edu.vn>
2023-04-23 02:19:45 +0700

    feat: update withScreenSecurity

--- lms/lmsfrontend/src/component/common/NotAuthorzied.js
+++ lms/lmsfrontend/src/component/common/NotAuthorzied.js
@@ -1,41 +1,23 @@
-import {Grid, Typography} from "@material-ui/core";
-import {makeStyles} from "@material-ui/core/styles";
-import ErrorIcon from "@material-ui/icons/Error";
-import React from "react";
-
-const useStyles = makeStyles((theme) => ({
-  root: {
-    padding: theme.spacing(4),
-  },
-  content: {
-    paddingTop: 150,
-    textAlign: "center",
-  },
-}));
+import ErrorIcon from "@mui/icons-material/Error";
+import { Stack, Typography } from "@mui/material";
 
 const NotAuthorized = () => {
-  const classes = useStyles();
-
   return (
-    <div className={classes.root}>
-      <Grid
-        container
-        justify="center"
-        spacing={4}
-        style={{ backgroundColor: "#ffe6e6" }}
-      >
-        <Grid item lg={6} xs={12}>
-          <div className={classes.content}>
-            <ErrorIcon color="error" fontSize="large" />
-            <Typography variant="h4">You need permissions</Typography>
-            <Typography variant="h6">
-              You do not have permission to access this component. Contact
-              administrator if you need help.
-            </Typography>
-          </div>
-        </Grid>
-      </Grid>
-    </div>
+    <Stack
+      direction="column"
+      alignItems="center"
+      justifyContent="center"
+      bgcolor={"#ffe6e6"}
+      height={`calc(100vh - 112px)`}
+      minWidth={500}
+    >
+      <ErrorIcon color="error" fontSize="large" />
+      <Typography variant="h4">You need permissions</Typography>
+      <Typography variant="h6" textAlign={"center"}>
+        You do not have permission to access this component. Please contact
+        administrator if you need help.
+      </Typography>
+    </Stack>
   );
 };
 
