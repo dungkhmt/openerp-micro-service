77eac9ca04a6828a151654aa91c63047bfee3ad4
NguyenDuy-Tien <tien.nd194857@sis.hust.edu.vn>
2023-04-23 02:19:10 +0700

    feat: add statistics & range scaling for ContestResultDistribution

--- lms/lmsfrontend/src/component/education/programmingcontestFE/ContestResultDistribution.js
+++ lms/lmsfrontend/src/component/education/programmingcontestFE/ContestResultDistribution.js
@@ -1,14 +1,31 @@
-import {Box, Divider, LinearProgress, TextField, Typography} from "@mui/material";
+import {Box, Divider, Grid, LinearProgress, TextField, Typography} from "@mui/material";
 import React, {useEffect, useState} from "react";
 import {request} from "../../../api";
 import {Chart} from "react-google-charts";
 import HustContainerCard from "../../common/HustContainerCard";
+import {LoadingButton} from "@mui/lab";
+import XLSX from "xlsx";
 
 export default function ContestResultDistribution(props) {
   const contestId = props.contestId;
   const [ranking, setRanking] = useState([]);
+  const [chartData, setChartData] = useState([]);
   const [maxPoint, setMaxPoint] = useState(0);
   const [numberOfBins, setNumberOfBins] = useState(10);
+
+  const [scaleStart, setScaleStart] = useState(0);
+  const [scaleEnd, setScaleEnd] = useState(100);
+
+  const defaultStats = {
+    "Count": 0,
+    "Highest": 0,
+    "Lowest": 0,
+    "Average": 0,
+    "Median": 0,
+    "Std Dev": 0
+  }
+
+  const [statistics, setStatistics] = useState(defaultStats);
 
   const [loading, setLoading] = useState(false);
 
@@ -25,15 +42,91 @@ export default function ContestResultDistribution(props) {
         setMaxPoint(Math.max(...data.map(obj => obj.totalPoint), 0))
 
         let arr = data.map(obj => [obj.userId, obj.totalPoint]);
-        arr.unshift(['User', 'Point']);
         setRanking(arr);
+
+        const chartArr = [...arr]
+        chartArr.unshift(['User', 'Point']);
+        setChartData(chartArr);
       }
     ).then(() => setLoading(false));
+  }
+
+  const handleScale = () => {
+    const initial_points = ranking.map(item => item[1]);
+    const min_point = Math.min(...initial_points);
+    const max_point = Math.max(...initial_points);
+
+    const initial_range = max_point - min_point;
+
+    const new_range = scaleEnd - scaleStart;
+
+    const rankingClone = [...ranking]
+    rankingClone.forEach(item => {
+      const old_point = item[1];
+      const new_point = (old_point - min_point) * (new_range / initial_range) + scaleStart;
+      item[1] = new_point;
+    });
+
+    setRanking(rankingClone);
+
+    const chartArr = [...rankingClone]
+    chartArr.unshift(['User', 'Point']);
+    setChartData(chartArr);
+  }
+
+  const handleExport = () => {
+    const wb = XLSX.utils.book_new();
+
+    const ws1 = XLSX.utils.aoa_to_sheet(ranking);
+    XLSX.utils.book_append_sheet(wb, ws1, "Result");
+
+    const ws2 = XLSX.utils.json_to_sheet([statistics]);
+    XLSX.utils.book_append_sheet(wb, ws2, "Insights");
+
+    // Write the workbook to a file
+    XLSX.writeFile(wb, "ContestResult-"+ contestId + ".xlsx");
   }
 
   useEffect(() => {
     getRanking();
   }, []);
+
+  useEffect(() => {
+    if (ranking && ranking.length > 0) {
+      let insights = defaultStats;
+
+      insights["Count"] = ranking.length;
+
+      const points = ranking.map((row) => row[1]);
+
+      const sum = points.reduce((acc, val) => acc + val);
+
+      const avg = sum / points.length;
+      insights["Average"] = avg.toFixed(2);
+
+      const max = Math.max(...points);
+      insights["Highest"] = max;
+
+      const min = Math.min(...points);
+      insights["Lowest"] = min;
+
+      const sortedPoints = points.slice().sort((a, b) => a - b);
+      const mid = Math.floor(sortedPoints.length / 2);
+      const median =
+        sortedPoints.length % 2 === 0
+          ? (sortedPoints[mid - 1] + sortedPoints[mid]) / 2
+          : sortedPoints[mid];
+      insights["Median"] = median.toFixed(2);
+
+      const variance =
+        points.reduce((acc, val) => acc + (val - avg) ** 2, 0) / points.length;
+
+      const stdDev = Math.sqrt(variance);
+      insights["Std Dev"] = stdDev.toFixed(2);
+
+      setStatistics(insights);
+    }
+  }, [ranking])
 
   const options = {
     title: 'Score Distribution in Contest',
@@ -64,36 +157,84 @@ export default function ContestResultDistribution(props) {
       {
         ranking.length > 0 &&
         <Box sx={{width: "100%", display: "flex", flexDirection: "row"}}>
-          <Box sx={{width: "84%"}}>
+          <Box sx={{width: "80%"}}>
             <Chart
               chartType="Histogram"
               width="100%"
-              height="400px"
-              data={ranking}
+              height="380px"
+              data={chartData}
               options={options}
             />
           </Box>
 
           <Divider orientation="vertical" flexItem/>
-          <Box sx ={{width: "15%", marginLeft: "18px"}}>
-            <Typography
-              fontWeight="600"
-              variant="h6"
-              component="div"
-              sx={{marginBottom: "18px"}}
-              color="#00acc1"
-            >
-              Options
-            </Typography>
-            <TextField
-              label={"Number of bins (about)"}
-              value={numberOfBins}
-              type={"number"}
-              onChange={(event) => {
-                setNumberOfBins(event.target.value);
-              }}
-              sx={{width: "100%"}}
-            />
+          <Box sx={{width: "18%", marginLeft: "18px"}}>
+            <Box sx={{marginBottom: "12px", display: "flex", flexDirection: "row", justifyContent: "space-between"}}>
+              <TextField
+                label={"From"}
+                value={scaleStart}
+                type={"number"}
+                onChange={(event) => {
+                  setScaleStart(parseFloat(event.target.value));
+                }}
+                sx={{marginRight: "8px"}}
+              />
+              <TextField
+                label={"To"}
+                value={scaleEnd}
+                type={"number"}
+                onChange={(event) => {
+                  setScaleEnd(parseFloat(event.target.value));
+                }}
+                sx={{marginRight: "12px"}}
+              />
+              <LoadingButton
+                variant="contained"
+                color={"primary"}
+                onClick={handleScale}
+                loading={loading}
+              >
+                Scale
+              </LoadingButton>
+            </Box>
+            <Box sx={{marginBottom: "24px", display: "flex", flexDirection: "row", justifyContent: "space-between"}}>
+              <LoadingButton
+                variant="contained"
+                color={"success"}
+                onClick={handleExport}
+                loading={loading}
+                sx={{width: "100%"}}
+              >
+                Export
+              </LoadingButton>
+            </Box>
+
+            <Box sx={{marginBottom: "24px"}}>
+              <TextField
+                label={"Number of bins (about)"}
+                value={numberOfBins}
+                type={"number"}
+                onChange={(event) => {
+                  setNumberOfBins(parseInt(event.target.value));
+                }}
+                sx={{width: "100%"}}
+              />
+            </Box>
+
+            <HustContainerCard title={"Insights"}>
+              <Grid container rowSpacing={1} columnSpacing={{xs: 1, sm: 2, md: 3}}>
+                {Object.keys(statistics).map((idx) => {
+                  return <>
+                    <Grid item xs={8}>
+                      <Typography color="#00acc1">{idx}:</Typography>
+                    </Grid>
+                    <Grid item xs={4}>
+                      <Typography sx={{fontStyle: "italic"}}>{statistics[idx]}</Typography>
+                    </Grid>
+                  </>
+                })}
+              </Grid>
+            </HustContainerCard>
           </Box>
 
         </Box>
