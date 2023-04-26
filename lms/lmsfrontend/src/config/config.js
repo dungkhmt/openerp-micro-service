7dcd7bb40b82dceb62364a7d36c61b1d9cec5f9d
dungkhmt <dungkhmt@gmail.com>
2023-04-26 21:31:36 +0700

    update config.js in lms frontend

--- lms/lmsfrontend/src/config/config.js
+++ lms/lmsfrontend/src/config/config.js
@@ -13,3 +13,4 @@ const dev = {
 };
 
 export const config = process.env.NODE_ENV === "development" ? dev : prod;
+export const API_URL = process.env.NODE_ENV === "development" ? dev.url.API_URL : prod.url.API_URL;
