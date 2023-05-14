c63558d72c880bfb2840a1a5cfbde4b1d2d0fdc4
QuangHa0110 <quanghadang08@gmail.com>
2023-05-14 22:13:30 +0700

    [DB] update schema.sql

--- code-editor-online/openerp-code-editor-server/src/main/java/db/schema.sql
+++ code-editor-online/openerp-code-editor-server/src/main/java/db/schema.sql
@@ -4,6 +4,8 @@ create table code_editor_room (
 	room_master_id varchar(255) not null,
 	create_date timestamp default current_timestamp,
 	edit_date timestamp default current_timestamp,
+	isPublic bool not null default false,
+	access_permission varchar(255) default 'VIEWER',
 	constraint pk_code_editor_room primary key (id)
 );
 
@@ -14,7 +16,15 @@ create table code_editor_source(
 	room_id uuid not null,
 	create_date timestamp default current_timestamp,
 	edit_date timestamp default current_timestamp,
-	edit_by varchar(255)
+	edit_by varchar(255),
 	constraint pk_code_editor_source primary key (id),
 	constraint fk_code_editor_source__code_editor_room_id foreign key (room_id) references code_editor_room (id)
+)
+
+create table code_editor_shared_user_room(
+	room_id uuid not null,
+	user_id varchar(255) not null,
+	constraint pk_code_editor_shared_user_room primary key (room_id, user_id),
+	constraint fk_code_editor_shared_user_room__room_id foreign key (room_id) references code_editor_room (id),
+	constraint fk_code_editor_shared_user_room__user_id foreign key (user_id) references user_login (user_login_id)
 )
