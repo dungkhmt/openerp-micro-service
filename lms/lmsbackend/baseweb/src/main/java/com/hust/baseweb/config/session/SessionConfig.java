94c5ca3dfca927047530f0909732d5e97d654fd0
NguyenDuy-Tien <tien.nd194857@sis.hust.edu.vn>
2023-05-20 00:15:34 +0700

    refactor: remove Redis session - lmsbackend

--- lms/lmsbackend/baseweb/src/main/java/com/hust/baseweb/config/session/SessionConfig.java
+++ lms/lmsbackend/baseweb/src/main/java/com/hust/baseweb/config/session/SessionConfig.java
@@ -7,15 +7,11 @@ import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
 import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
 import org.springframework.data.redis.core.RedisOperations;
 import org.springframework.data.redis.core.RedisTemplate;
-import org.springframework.session.config.annotation.web.http.EnableSpringHttpSession;
-import org.springframework.session.data.redis.RedisSessionRepository;
-import org.springframework.session.web.http.HeaderHttpSessionIdResolver;
-import org.springframework.session.web.http.HttpSessionIdResolver;
 
 import java.time.Duration;
 
 @Configuration
-@EnableSpringHttpSession
+//@EnableSpringHttpSession
 public class SessionConfig {
 
     @Value("${spring.redis.host}")
@@ -45,22 +41,22 @@ public class SessionConfig {
         return RedisSerializationBuilder.getSnappyRedisTemplate(redisConnectionFactory(), Object.class);
     }
 
-    @Bean
-    public RedisSessionRepository sessionRepository(RedisTemplate<String, Object> redisTemplate) {
-        RedisSessionRepository redisSessionRepository = new RedisSessionRepository(redisTemplate);
-        redisSessionRepository.setDefaultMaxInactiveInterval(Duration.ofHours(24));
-        return redisSessionRepository;
-    }
+//    @Bean
+//    public RedisSessionRepository sessionRepository(RedisTemplate<String, Object> redisTemplate) {
+//        RedisSessionRepository redisSessionRepository = new RedisSessionRepository(redisTemplate);
+//        redisSessionRepository.setDefaultMaxInactiveInterval(Duration.ofHours(24));
+//        return redisSessionRepository;
+//    }
 
-    /**
-     * Customize Spring Session’s HttpSession integration to use HTTP headers X-Auth-Token
-     * to convey the current session information instead of cookies,
-     * <a href="https://docs.spring.io/spring-session/docs/current/reference/html5/guides/java-rest.html">reference</a>
-     *
-     * @return
-     */
-    @Bean
-    public HttpSessionIdResolver httpSessionIdResolver() {
-        return HeaderHttpSessionIdResolver.xAuthToken();
-    }
+//    /**
+//     * Customize Spring Session’s HttpSession integration to use HTTP headers X-Auth-Token
+//     * to convey the current session information instead of cookies,
+//     * <a href="https://docs.spring.io/spring-session/docs/current/reference/html5/guides/java-rest.html">reference</a>
+//     *
+//     * @return
+//     */
+//    @Bean
+//    public HttpSessionIdResolver httpSessionIdResolver() {
+//        return HeaderHttpSessionIdResolver.xAuthToken();
+//    }
 }
