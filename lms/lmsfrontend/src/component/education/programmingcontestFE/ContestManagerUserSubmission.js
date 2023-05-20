dca5675544dc07c65712c9bdda7c2801b0c41324
NguyenDuy-Tien <tien.nd194857@sis.hust.edu.vn>
2023-05-20 23:37:42 +0700

    chore: remove mistyped code

--- lms/lmsfrontend/src/component/education/programmingcontestFE/ContestManagerUserSubmission.js
+++ lms/lmsfrontend/src/component/education/programmingcontestFE/ContestManagerUserSubmission.js
@@ -90,7 +90,6 @@ export default function ContestManagerUserSubmission(props) {
             onClick={() => {
               handleRejudge(rowData.contestSubmissionId);
             }}
-            sx={{marginBottom: "12px"}}
           >
             {" "}
             REJUDGE{" "}
