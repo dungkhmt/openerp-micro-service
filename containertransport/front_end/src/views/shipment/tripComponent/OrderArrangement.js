83c72d937e2b6761688b19672df87492f09c6d6a
DinhVu3671 <vuthptnhoquana@gmail.com>
2023-05-24 08:38:03 +0700

    update layout shipment

--- containertransport/front_end/src/views/shipment/tripComponent/OrderArrangement.js
+++ containertransport/front_end/src/views/shipment/tripComponent/OrderArrangement.js
@@ -13,8 +13,8 @@ import ModalTripItem from "../tripCreate/ModalTripItem";
 
 const TripItem = ({ index, item, facilities, setFacilities }) => {
     const [open, setOpen] = useState(false);
-    const [arrivalTime, setArrivalTime] = useState(item.arrivalTime);
-    const [departureTime, setDepartureTime] = useState(item.departureTime);
+    const [arrivalTime, setArrivalTime] = useState(item?.arrivalTime);
+    const [departureTime, setDepartureTime] = useState(item?.departureTime);
 
     const handleChangeTime = (time, id, type) => {
         facilities.map((facility, i) => {
@@ -33,7 +33,7 @@ const TripItem = ({ index, item, facilities, setFacilities }) => {
         //   setCounters(nextCounters);
     }
     return (
-        <Draggable key={item.id} index={index} draggableId={item.id}>
+        <Draggable key={item?.id} index={index} draggableId={item?.id}>
             {(provided, snapshot) => (
                 <div
                     ref={provided.innerRef}
@@ -54,7 +54,7 @@ const TripItem = ({ index, item, facilities, setFacilities }) => {
                     className="item-facility"
                     onClick={() => setOpen(!open)}
                 >
-                    <Box>{item.orderCode} - {item.facilityName} - {item.action}</Box>
+                    <Box>{item?.orderCode} - {item?.facilityName} - {item?.action}</Box>
 
                     {open ?
                         (<Box>
@@ -69,8 +69,8 @@ const TripItem = ({ index, item, facilities, setFacilities }) => {
                                         <LocalizationProvider dateAdapter={AdapterDayjs}>
                                             <DemoContainer components={['DateTimePicker']}>
                                                 <DateTimePicker label="Early Delivery Time"
-                                                    defaultValue={item.arrivalTime ? dayjs(new Date(item.arrivalTime)) : null}
-                                                    onChange={(e) => handleChangeTime((new Date(e)).getTime(), item.id, "arrivalTime")}
+                                                    defaultValue={item?.arrivalTime ? dayjs(new Date(item?.arrivalTime)) : null}
+                                                    onChange={(e) => handleChangeTime((new Date(e)).getTime(), item?.id, "arrivalTime")}
                                                 />
                                             </DemoContainer>
                                         </LocalizationProvider>
@@ -84,8 +84,8 @@ const TripItem = ({ index, item, facilities, setFacilities }) => {
                                         <LocalizationProvider dateAdapter={AdapterDayjs}>
                                             <DemoContainer components={['DateTimePicker']}>
                                                 <DateTimePicker label="Early Delivery Time"
-                                                    defaultValue={item.departureTime ? dayjs(new Date(item.departureTime)) : null}
-                                                    onChange={(e) => handleChangeTime((new Date(e)).getTime(), item.id, "departureTime")}
+                                                    defaultValue={item?.departureTime ? dayjs(new Date(item?.departureTime)) : null}
+                                                    onChange={(e) => handleChangeTime((new Date(e)).getTime(), item?.id, "departureTime")}
                                                 />
                                             </DemoContainer>
                                         </LocalizationProvider>
@@ -105,6 +105,7 @@ const OrderArrangement = ({ ordersSelect, setTripItem, truckSelected, tripId }) 
     const [facilitiesFinal, setFacilitiesFinal] = useState([]);
     const [open, setOpen] = useState(false);
     const [addTripItem, setAddTripItem] = useState();
+    const [truckItem, setTruckItem] = useState([]);
 
     const handleModal = () => {
         setOpen(!open);
@@ -136,90 +137,119 @@ const OrderArrangement = ({ ordersSelect, setTripItem, truckSelected, tripId }) 
                 longitude: truckSelected?.facilityResponsiveDTO?.longitude,
                 latitude: truckSelected?.facilityResponsiveDTO?.latitude,
                 arrivalTime: null,
-                departureTime: null
+                departureTime: null,
+                type: "truck"
             }
             facilitiesTmp.push(startPoint);
+            setTruckItem(facilitiesTmp);
             facilitiesTmp = facilitiesTmp.concat(facilities);
             setFacilitiesFinal(facilitiesTmp);
         }
         if (!truckSelected) {
-            // facilitiesTmp = facilitiesFinal;
-            // facilitiesTmp.splice(0, 1);
-            setFacilitiesFinal(facilities);
+            facilitiesTmp = facilitiesFinal.filter((faci) => (faci.action != "DEPART"));
+            facilitiesTmp = facilitiesTmp.filter((faci) => (faci.action != "STOP"));
+            setFacilities(facilitiesTmp);
+            setFacilitiesFinal(facilitiesTmp);
+            setTruckItem([]);
         }
     }, [truckSelected]);
-    console.log("facilities154", facilitiesFinal);
+    
     useEffect(() => {
-        let facilitiesTmp = [];
-        let startPoints = [];
-        ordersSelect.forEach((item) => {
-            let fromFacility = {
-                id: item?.id + "F1",
-                facilityId: item?.fromFacility.facilityId,
-                facilityName: item?.fromFacility.facilityName,
-                facilityCode: item?.fromFacility.facilityCode,
-                earlyTime: item?.earlyPickupTime,
-                lateTime: item?.latePickupTime,
-                action: "PICKUP-CONTAINER",
-                orderCode: item?.orderCode,
-                orderId: item.id,
-                container: item?.containers[0],
-                longitude: item?.fromFacility.longitude,
-                latitude: item?.fromFacility.latitude,
-                arrivalTime: null,
-                departureTime: null
-            }
-            let toFacility = {
-                id: item?.id + "F2",
-                facilityId: item?.toFacility.facilityId,
-                facilityName: item?.toFacility.facilityName,
-                facilityCode: item?.toFacility.facilityCode,
-                earlyTime: item?.earlyDeliveryTime,
-                lateTime: item?.lateDeliveryTime,
-                action: "DELIVERY-CONTAINER",
-                orderCode: item?.orderCode,
-                orderId: item.id,
-                container: item?.containers[0],
-                longitude: item?.toFacility.longitude,
-                latitude: item?.toFacility.latitude,
-                arrivalTime: null,
-                departureTime: null
-            }
-            facilitiesTmp.push(fromFacility);
-            facilitiesTmp.push(toFacility);
-        });
-        if (truckSelected) {
-            let startPoint = {
-                id: truckSelected?.id + "S1",
-                facilityId: truckSelected?.facilityResponsiveDTO?.facilityId,
-                facilityName: truckSelected?.facilityResponsiveDTO?.facilityName,
-                facilityCode: truckSelected?.facilityResponsiveDTO?.facilityCode,
-                action: "DEPART",
-                orderCode: truckSelected?.truckCode,
-                longitude: truckSelected?.facilityResponsiveDTO?.longitude,
-                latitude: truckSelected?.facilityResponsiveDTO?.latitude,
-                arrivalTime: null,
-                departureTime: null
-            }
-            startPoints.push(startPoint);
+        let facilitiesTmp = facilitiesFinal;
+        console.log("facilitiesBefore111", truckItem);
+        if (truckItem.length > 0){
+            facilitiesTmp.shift()
         }
-        setFacilities(facilitiesTmp);
-        startPoints = startPoints.concat(facilitiesTmp);
-        setFacilitiesFinal(startPoints);
+        console.log("facilitiesBefore", facilitiesTmp);
+        let facilitiesTmp2 = [];
+        if (ordersSelect) {
+            ordersSelect.forEach((item) => {
+                let check = false;
+                // let facilitiesOrder = facilities.filter((item) => item.type = "order")
+                // neu da ton tai order trong router
+                facilitiesTmp?.forEach((faci) => {
+                    if (faci.orderId == item.id) {
+                        check = true;
+                    }
+                })
+                // neu order chua ton tai trong lo trinh
+                if (!check) {
+                    let fromFacility = {
+                        id: item?.id + "F1",
+                        facilityId: item?.fromFacility.facilityId,
+                        facilityName: item?.fromFacility.facilityName,
+                        facilityCode: item?.fromFacility.facilityCode,
+                        earlyTime: item?.earlyPickupTime,
+                        lateTime: item?.latePickupTime,
+                        action: "PICKUP-CONTAINER",
+                        orderCode: item?.orderCode,
+                        orderId: item.id,
+                        container: item?.containers[0],
+                        longitude: item?.fromFacility.longitude,
+                        latitude: item?.fromFacility.latitude,
+                        arrivalTime: null,
+                        departureTime: null,
+                        type: "order"
+                    }
+                    let toFacility = {
+                        id: item?.id + "F2",
+                        facilityId: item?.toFacility.facilityId,
+                        facilityName: item?.toFacility.facilityName,
+                        facilityCode: item?.toFacility.facilityCode,
+                        earlyTime: item?.earlyDeliveryTime,
+                        lateTime: item?.lateDeliveryTime,
+                        action: "DELIVERY-CONTAINER",
+                        orderCode: item?.orderCode,
+                        orderId: item.id,
+                        container: item?.containers[0],
+                        longitude: item?.toFacility.longitude,
+                        latitude: item?.toFacility.latitude,
+                        arrivalTime: null,
+                        departureTime: null,
+                        type: "order"
+                    }
+                    facilitiesTmp.push(fromFacility);
+                    facilitiesTmp.push(toFacility);
+                }
+            });
+            facilitiesTmp2 = facilitiesTmp;
+            console.log("facilitiesTmp", facilitiesTmp);
+            facilitiesTmp.forEach((faci, index) => {
+                if(faci.type == "order") {
+                    let check = false;
+                    ordersSelect.forEach((item) => {
+                        if (item.id == faci.orderId) {
+                            check = true;
+                        }
+                    });
+                    if (!check) {
+                        facilitiesTmp2 = facilitiesTmp2.filter((req) => req.orderId != faci.orderId);
+                    }
+                }
+            })
+            console.log("facilitiesTmp2", facilitiesTmp2);
+        }
+        setFacilities(facilitiesTmp2);
+        let facilitiesAdd = truckItem?.concat(facilitiesTmp2);
+        setFacilitiesFinal(facilitiesAdd);
         if (!tripId) {
-            setTripItem(startPoints)
+            setTripItem(facilitiesAdd)
         }
-    }, [ordersSelect])
 
+    }, [ordersSelect])
+    console.log("facilities151", facilitiesFinal);
     console.log("truckSelected", truckSelected);
     const handleAddTripItem = () => {
         setOpen(true);
     }
-    useEffect(() => {
-        let facilitiesTmp = facilitiesFinal
-        facilitiesTmp = facilitiesTmp.concat(addTripItem);
-        // setFacilitiesFinal(facilitiesTmp);
-    }, addTripItem)
+    // useEffect(() => {
+    //     let facilitiesTmp = facilitiesFinal
+    //     if (addTripItem) {
+    //         facilitiesTmp = facilitiesTmp.concat(addTripItem);
+    //         console.log("facilitiesTmp", facilitiesTmp);
+    //         setFacilitiesFinal(facilitiesTmp);
+    //     }
+    // }, addTripItem)
     return (
         <Box className="facility-arrangment">
             <Box className="facility-arrangment-text">
