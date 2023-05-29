1e884a8d3063447673b2a80f9c21e805519a708b
Lê Anh Tuấn <anhtuan0126104@gmail.com>
2023-05-29 10:42:03 +0700

    [chore] change config: osmFilePath

--- wms/wms-backend/src/main/java/com/hust/wmsbackend/management/auto/impl/GraphHopperCalculator.java
+++ wms/wms-backend/src/main/java/com/hust/wmsbackend/management/auto/impl/GraphHopperCalculator.java
@@ -9,8 +9,7 @@ import com.hust.wmsbackend.management.auto.DistanceCalculator;
 import com.hust.wmsbackend.management.entity.Warehouse;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.commons.math3.util.Precision;
-import org.springframework.core.io.ClassPathResource;
-import org.springframework.core.io.Resource;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
 import java.math.BigDecimal;
@@ -22,10 +21,17 @@ public class GraphHopperCalculator implements DistanceCalculator {
 
     private final GraphHopper graphHopper;
 
-    public GraphHopperCalculator() {
+    public GraphHopperCalculator(@Value("${osm-file-path}") String osmFilePath) {
         this.graphHopper = new GraphHopper();
-        graphHopper.setProfiles(new Profile("car").setVehicle("car").setWeighting("fastest").setTurnCosts(false));
-        graphHopper.setOSMFile(loadVietnamRouteResource());
+
+        graphHopper.setProfiles(
+                new Profile("car")
+                        .setVehicle("car")
+                        .setWeighting("fastest")
+                        .setTurnCosts(false)
+        );
+
+        graphHopper.setOSMFile(osmFilePath);
         graphHopper.setGraphHopperLocation("target/routing-graph-vietnam-latest-cache");
         graphHopper.importOrLoad();
     }
@@ -35,13 +41,13 @@ public class GraphHopperCalculator implements DistanceCalculator {
         Map<UUID, Double> map = new HashMap<>();
         for (Warehouse warehouse : warehouses) {
             GHRequest request = new GHRequest(roundBigDecimal(warehouse.getLatitude()), roundBigDecimal(warehouse.getLongitude()),
-                                              roundDouble(cusAddLat), roundDouble(cusAddLon)).setProfile("car")
-                                                                                             .setLocale(Locale.US);
+                    roundDouble(cusAddLat), roundDouble(cusAddLon)).setProfile("car")
+                    .setLocale(Locale.US);
             GHResponse response = graphHopper.route(request);
             ResponsePath path = response.getBest(); // try catch here
             if (path == null) {
                 log.warn(String.format("Not path found for warehouse %s to CustomerAddress(lon:%f, lat:%f)",
-                                       warehouse.getWarehouseId(), cusAddLon, cusAddLat));
+                        warehouse.getWarehouseId(), cusAddLon, cusAddLat));
                 continue;
             }
             double distance = path.getDistance();
@@ -53,22 +59,14 @@ public class GraphHopperCalculator implements DistanceCalculator {
     @Override
     public ResponsePath calculate(BigDecimal fromLat, BigDecimal fromLon, BigDecimal toLat, BigDecimal toLon) {
         GHRequest request = new GHRequest(roundBigDecimal(fromLat), roundBigDecimal(fromLon),
-                                          roundBigDecimal(toLat), roundBigDecimal(toLon))
-            .setProfile("car").setLocale(Locale.US);
+                roundBigDecimal(toLat), roundBigDecimal(toLon))
+                .setProfile("car").setLocale(Locale.US);
         GHResponse response = graphHopper.route(request);
         ResponsePath path = response.getBest();
         if (path == null) {
             throw new PathNotFoundException(String.format("Not path found from Point(lat:%f, lon:%f) to Point(lat:%f, lon:%f)", fromLat, fromLon, toLat, toLon));
         }
         return path;
-    }
-
-    private String loadVietnamRouteResource() {
-        Resource resource =  new ClassPathResource("osm/vietnam-latest.osm.pbf");
-        if (resource.isFile()) {
-            return resource.getFilename();
-        }
-        throw new RuntimeException("Missing viet nam route resource pbf file");
     }
 
     private double roundBigDecimal(BigDecimal b) {
