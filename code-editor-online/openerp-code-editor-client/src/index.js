49d424b69bf4a8af0ef15c679e8740e225b043c0
Lê Anh Tuấn <anhtuan0126104@gmail.com>
2023-06-03 13:09:05 +0700

    [CI] test trigger CI

--- code-editor-online/openerp-code-editor-client/src/index.js
+++ code-editor-online/openerp-code-editor-client/src/index.js
@@ -1,10 +1,9 @@
-import React from "react";
 import ReactDOM from "react-dom/client";
+import { Provider } from "react-redux";
+import { store } from "store/store";
 import App from "./App";
 import "./index.css";
 import reportWebVitals from "./reportWebVitals";
-import { Provider } from "react-redux";
-import { store } from "store/store";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
