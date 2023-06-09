9d9fecae37d5182139809a385e97dc572d1c8743
DinhVu3671 <vuthptnhoquana@gmail.com>
2023-06-09 17:49:38 +0700

    update algorithms

--- containertransport/back_end/src/main/java/openerp/containertransport/constants/Constants.java
+++ containertransport/back_end/src/main/java/openerp/containertransport/constants/Constants.java
@@ -18,4 +18,53 @@ public class Constants implements Serializable {
             this.value = value;
         }
     }
+
+    @Getter
+    public enum TruckStatus {
+
+        AVAILABLE("AVAILABLE"),
+        SCHEDULED("SCHEDULED"),
+        UNAVAILABLE("UNAVAILABLE"),
+        EXECUTING("EXECUTING");
+        private final String status;
+        TruckStatus(String status) {
+            this.status = status;
+        }
+    }
+
+    @Getter
+    public enum TrailerStatus {
+        AVAILABLE("AVAILABLE"),
+        SCHEDULED("SCHEDULED"),
+        UNAVAILABLE("UNAVAILABLE"),
+        EXECUTING("EXECUTING");
+        private final String status;
+        TrailerStatus(String status) {
+            this.status = status;
+        }
+    }
+
+    @Getter
+    public enum OrderStatus {
+        ORDERED("ORDERED"),
+        SCHEDULED("SCHEDULED"),
+        DONE("DONE"),
+        EXECUTING("EXECUTING");
+        private final String status;
+        OrderStatus(String status) {
+            this.status = status;
+        }
+    }
+
+    @Getter
+    public enum ContainerStatus {
+        ORDERED("ORDERED"),
+        SCHEDULED("SCHEDULED"),
+        AVAILABLE("AVAILABLE"),
+        EXECUTING("EXECUTING");
+        private final String status;
+        ContainerStatus(String status) {
+            this.status = status;
+        }
+    }
 }
