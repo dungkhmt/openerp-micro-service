37430d9dc4e7e88871d7c121d60fca50a6a96f4c
NguyenDuy-Tien <tien.nd194857@sis.hust.edu.vn>
2023-06-26 13:48:00 +0700

    enable scheduling - lmsbackend

--- lms/lmsbackend/baseweb/src/main/java/com/hust/baseweb/BasewebApplication.java
+++ lms/lmsbackend/baseweb/src/main/java/com/hust/baseweb/BasewebApplication.java
@@ -4,10 +4,12 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 
 @EnableAsync
+@EnableScheduling
 @EnableCaching
 @EnableWebSecurity
 @EnableGlobalMethodSecurity(securedEnabled = true)
