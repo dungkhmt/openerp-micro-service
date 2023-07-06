f4e4f02a4de847a7707678175cb3edf661ffaab1
NguyenDuy-Tien <tien.nd194857@sis.hust.edu.vn>
2023-07-07 00:56:03 +0700

    set default contest problem role when adding - lmsfrontend

--- lms/lmsfrontend/src/component/education/programmingcontestFE/AddMemberProblemDialog.js
+++ lms/lmsfrontend/src/component/education/programmingcontestFE/AddMemberProblemDialog.js
@@ -1,28 +1,16 @@
-//import { makeStyles } from "@mui/styles"; //"@material-ui/core/styles";
-import { makeStyles } from "@material-ui/core/styles";
+import {MenuItem, TextField} from "@mui/material";
+import {HustModal} from "erp-hust/lib/HustModal/HustModal";
+import React, {useState} from "react";
 
-import { MenuItem, TextField } from "@mui/material";
-import PrimaryButton from "component/button/PrimaryButton";
-import TertiaryButton from "component/button/TertiaryButton";
-import { HustModal } from "erp-hust/lib/HustModal/HustModal";
-import React, { useState } from "react";
-
-const useStyles = makeStyles((theme) => ({
-  dialogContent: {
-    minWidth: 480,
-    minHeight: 64,
-  },
-  btn: { margin: "4px 8px" },
-}));
 export default function AddMemberProblemDialog(props) {
   const { open, onClose, onUpdateInfo, selectedUserId, rolesList } = props;
-  const [selectRole, setSelectRole] = useState(null);
-  console.log("rolesList", rolesList);
+  const [selectRole, setSelectRole] = useState(rolesList[0]);
+
   return (
     <HustModal
       open={open}
       onClose={onClose}
-      title={`Add member ${selectedUserId}`}
+      title={`Assign role for user:  ${selectedUserId}`}
       textOk="Add"
       onOk={() => onUpdateInfo(selectRole, selectedUserId)}
     >
