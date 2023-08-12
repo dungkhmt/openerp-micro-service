58e86341d13ee13a50c0ac6e08e6c8ad1ce8d380
NguyenDuy-Tien <tien.nd194857@sis.hust.edu.vn>
2023-08-12 10:21:57 +0700

    fix typo

--- lms/lmsfrontend/src/config/config.js
+++ lms/lmsfrontend/src/config/config.js
@@ -1,15 +1,15 @@
 const prod = {
   url: {
-    // KEYCLOAK_BASE_URL: "https://lms.dailyopt.ai/iam",
     KEYCLOAK_BASE_URL: "https://lms.dailyopt.ai/iam",
+    // KEYCLOAK_BASE_URL: "https://openerp3.dailyopt.ai/iam",
     API_URL: "/api",
   },
 };
 
 const dev = {
   url: {
-    // KEYCLOAK_BASE_URL: "https://lms.dailyopt.ai/iam",
     KEYCLOAK_BASE_URL: "https://lms.dailyopt.ai/iam",
+    // KEYCLOAK_BASE_URL: "https://openerp3.dailyopt.ai/iam",
     API_URL: "http://localhost:8080/api",
   },
 };
