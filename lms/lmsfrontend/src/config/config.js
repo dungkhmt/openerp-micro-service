c4d013d5a393a01520d89ee010a54867e696e65a
NguyenDuy-Tien <tien.nd194857@sis.hust.edu.vn>
2023-09-03 22:54:05 +0700

    update realm lms dev

--- lms/lmsfrontend/src/config/config.js
+++ lms/lmsfrontend/src/config/config.js
@@ -8,8 +8,8 @@ const prod = {
 
 const dev = {
   url: {
-    KEYCLOAK_BASE_URL: "https://lms.dailyopt.ai/iam",
-    // KEYCLOAK_BASE_URL: "https://openerp3.dailyopt.ai/iam",
+    // KEYCLOAK_BASE_URL: "https://lms.dailyopt.ai/iam",
+    KEYCLOAK_BASE_URL: "https://openerp3.dailyopt.ai/iam",
     API_URL: "http://localhost:8080/api",
   },
 };
