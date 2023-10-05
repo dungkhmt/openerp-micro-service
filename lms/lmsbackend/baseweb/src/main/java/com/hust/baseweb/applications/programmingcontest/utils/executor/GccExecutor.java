c5f4146c352d0fa42f03ac7b5464004ae9cd7874
NguyenDuy-Tien <tien.nd194857@sis.hust.edu.vn>
2023-10-05 14:54:44 +0700

    add -lm option for C compiler

--- lms/lmsbackend/baseweb/src/main/java/com/hust/baseweb/applications/programmingcontest/utils/executor/GccExecutor.java
+++ lms/lmsbackend/baseweb/src/main/java/com/hust/baseweb/applications/programmingcontest/utils/executor/GccExecutor.java
@@ -9,7 +9,7 @@ import java.util.List;
 
 public class GccExecutor {
 
-    private static final String BUILD_COMMAND_C = "gcc -std=c17 -w -o main main.c";
+    private static final String BUILD_COMMAND_C = "gcc -std=c17 -w -o main main.c -lm";
     private static final String BUILD_COMMAND_CPP_11 = "g++ -std=c++11 -w -o main main.cpp";
     private static final String BUILD_COMMAND_CPP_14 = "g++ -std=c++14 -w -o main main.cpp";
     private static final String BUILD_COMMAND_CPP_17 = "g++ -std=c++17 -w -o main main.cpp";
