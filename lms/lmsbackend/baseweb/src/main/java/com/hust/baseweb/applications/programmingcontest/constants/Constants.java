ab27102626bb9da2ff9f6ca79a86d0b5b287eda1
NguyenDuy-Tien <tienduy2912@gmail.com>
2023-11-12 17:46:40 +0700

    fix error CI

--- lms/lmsbackend/baseweb/src/main/java/com/hust/baseweb/applications/programmingcontest/constants/Constants.java
+++ lms/lmsbackend/baseweb/src/main/java/com/hust/baseweb/applications/programmingcontest/constants/Constants.java
@@ -4,7 +4,11 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import org.apache.commons.lang3.RandomStringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @Configuration
 @AllArgsConstructor(onConstructor_ = @Autowired)
@@ -13,6 +17,15 @@ public class Constants {
 
     public static final String SPLIT_TEST_CASE = "testcasedone" + RandomStringUtils.randomAlphabetic(10);
     public static final String SOURCECODE_HEREDOC_DELIMITER = RandomStringUtils.randomAlphabetic(10);
+
+    private Map<String, Integer> MapLevelOrder = new HashMap<>();
+
+    @Bean
+    public void initConstants() {
+        MapLevelOrder.put("easy", 1);
+        MapLevelOrder.put("medium", 2);
+        MapLevelOrder.put("hard", 3);
+    }
 
     public enum RegistrationType {
         PENDING("PENDING"),
