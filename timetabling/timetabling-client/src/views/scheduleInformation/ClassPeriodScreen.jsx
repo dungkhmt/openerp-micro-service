ec2f61b946042a61db035f57ab74ac0f5fc69792
chienhoangvan <chienhoangvanp@gmail.com>
2024-01-03 21:28:19 +0700

    remove not need code

--- timetabling/timetabling-client/src/views/scheduleInformation/ClassPeriodScreen.jsx
+++ timetabling/timetabling-client/src/views/scheduleInformation/ClassPeriodScreen.jsx
@@ -1,87 +1,56 @@
-import React, { useEffect, useState, forwardRef } from "react";
+import * as React from 'react';
+import { useEffect, useState } from "react";
 import { request } from "../../api";
-import MaterialTable, { MTableToolbar } from "material-table";
-import AddBox from '@material-ui/icons/AddBox';
-import ArrowDownward from '@material-ui/icons/ArrowDownward';
-import Check from '@material-ui/icons/Check';
-import ChevronLeft from '@material-ui/icons/ChevronLeft';
-import ChevronRight from '@material-ui/icons/ChevronRight';
-import Clear from '@material-ui/icons/Clear';
-import DeleteOutline from '@material-ui/icons/DeleteOutline';
-import Edit from '@material-ui/icons/Edit';
-import FilterList from '@material-ui/icons/FilterList';
-import FirstPage from '@material-ui/icons/FirstPage';
-import LastPage from '@material-ui/icons/LastPage';
-import Remove from '@material-ui/icons/Remove';
-import SaveAlt from '@material-ui/icons/SaveAlt';
-import Search from '@material-ui/icons/Search';
-import ViewColumn from '@material-ui/icons/ViewColumn';
+import { DataGrid } from '@mui/x-data-grid';
+import { Box, Typography } from '@mui/material'
 
-function WeekDayScreen() {
+const columns = [
+    {
+        headerName: "Class Period ID",
+        field: "id",
+        width: 150
+    },
+    {
+        headerName: "Tiết học",
+        field: "classPeriod",
+        width: 150
+    },
+];
 
+export default function TimePerformanceScreen() {
     const [classPeriods, setClassPeriods] = useState([]);
+    const [dataChanged, setDataChanged] = useState(false);
+    const [refreshKey, setRefreshKey] = useState(0);
 
     useEffect(() => {
         request("get", "/class-period/get-all", (res) => {
             setClassPeriods(res.data);
         }).then();
-    }, [])
+    }, [refreshKey])
 
-    const columns = [
-        {
-            title: "Class Period ID",
-            field: "id",
-        },
-        {
-            title: "Tiết học",
-            field: "classPeriod",
-        }
-    ];
-
-    const tableIcons = {
-        Add: forwardRef((props, ref) => <AddBox {...props} ref={ref} />),
-        Check: forwardRef((props, ref) => <Check {...props} ref={ref} />),
-        Clear: forwardRef((props, ref) => <Clear {...props} ref={ref} />),
-        Delete: forwardRef((props, ref) => <DeleteOutline {...props} ref={ref} />),
-        DetailPanel: forwardRef((props, ref) => <ChevronRight {...props} ref={ref} />),
-        Edit: forwardRef((props, ref) => <Edit {...props} ref={ref} />),
-        Export: forwardRef((props, ref) => <SaveAlt {...props} ref={ref} />),
-        Filter: forwardRef((props, ref) => <FilterList {...props} ref={ref} />),
-        FirstPage: forwardRef((props, ref) => <FirstPage {...props} ref={ref} />),
-        LastPage: forwardRef((props, ref) => <LastPage {...props} ref={ref} />),
-        NextPage: forwardRef((props, ref) => <ChevronRight {...props} ref={ref} />),
-        PreviousPage: forwardRef((props, ref) => <ChevronLeft {...props} ref={ref} />),
-        ResetSearch: forwardRef((props, ref) => <Clear {...props} ref={ref} />),
-        Search: forwardRef((props, ref) => <Search {...props} ref={ref} />),
-        SortArrow: forwardRef((props, ref) => <ArrowDownward {...props} ref={ref} />),
-        ThirdStateCheck: forwardRef((props, ref) => <Remove {...props} ref={ref} />),
-        ViewColumn: forwardRef((props, ref) => <ViewColumn {...props} ref={ref} />)
-    };
+    function DataGridTitle() {
+        return (
+            <Box style={{ width: "100%", display: "flex", justifyContent: "center", alignItems: "center" }}>
+                <Typography variant="h5">Danh sách Tiết học</Typography>
+            </Box>
+        )
+    }
 
     return (
-        <div>
-            <MaterialTable
-                title={"Danh sách Tiết học"}
-                columns={columns}
-                data={classPeriods}
-                icons={tableIcons}
+        <div style={{ height: 500, width: '100%' }}>
+            <DataGrid
+                key={dataChanged}
                 components={{
-                    Toolbar: (props) => (
-                        <div style={{
-                            position: "relative"
-                        }}>
-                            <MTableToolbar {...props} />
-                            <div
-                                style={{ position: "absolute", top: "16px", right: "350px" }}
-                            >
-                            </div>
-                        </div>
+                    Toolbar: () => (
+                        <>
+                            <DataGridTitle />
+                        </>
                     ),
                 }}
+                rows={classPeriods}
+                columns={columns}
+                pageSize={10}
             />
         </div>
-
     );
 }
-
-export default WeekDayScreen;
