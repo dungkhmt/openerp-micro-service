a596d87b8c4d573d66513df460e57275d4111a63
NguyenDuy-Tien <tienduy2912@gmail.com>
2024-02-17 15:39:08 +0700

    update programming challenge - dashboard

--- lms/lmsfrontend/src/component/dashboard/DailyProgramming.jsx
+++ lms/lmsfrontend/src/component/dashboard/DailyProgramming.jsx
@@ -1,10 +1,9 @@
-import {Button, CardActions, CardContent, CardHeader, Divider, SvgIcon} from "@mui/material";
+import {CardContent, CardHeader} from "@mui/material";
 import Card from "@mui/material/Card";
 import Typography from "@mui/material/Typography";
-import {ArrowRightIcon} from "@heroicons/react/16/solid";
 import React, {useEffect, useState} from "react";
-import {warningNoti} from "../../utils/notification";
 import {problems} from "./problem-list";
+import Box from "@mui/material/Box";
 
 export default function DailyProgramming() {
 
@@ -17,17 +16,54 @@ export default function DailyProgramming() {
 
   return <Card elevation={5} sx={{borderRadius: "18px"}}>
     <CardHeader
-      title={<Typography variant="h6" color="#00acc1">Daily Programming Challenge</Typography>}
+      title={<Typography variant="h6" color="#00acc1">Challenge Problem</Typography>}
     />
     <CardContent sx={{paddingTop: 0}}>
       <Typography variant="subtitle1" color="#00acc1" paddingBottom="4px"
-                  sx={{fontSize: "18px"}}>{todayProblem.title}</Typography>
-      <Typography sx={{
+                  sx={{fontSize: "18px"}}>
+        Truck trailer scheduling for transporting containers
+      </Typography>
+
+      <Box sx={{display: "flex", paddingLeft: "20%", paddingY: "12px"}}>
+        <img
+          src={"assets/img/container-transportation.png"}
+          alt="programming-challenge"
+          width="60%"
+        />
+      </Box>
+
+      <Box sx={{
         textAlign: "justify",
-        fontStyle: "italic",
-        fontSize: "15px",
+        // fontStyle: "italic",
+        fontSize: "14px",
         opacity: 0.8
-      }}>{todayProblem.description}</Typography>
+      }}>
+        <Typography>
+          In the field of logistics and supply chain management, transporting goods between ports and warehouses is
+          essential, and this is done using containers. Customers place requests for moving goods either from warehouses
+          to ports or between different warehouses. In this process, trucks, trailers, and containers are distinct
+          entities. Here's how the process typically includes:
+        </Typography>
+        <ol>
+          <li><Typography>
+            A truck goes to a depot to pick up a trailer.
+          </Typography></li>
+          <li><Typography>
+            The truck with trailer attached (truck-trailer) heads to another depot to collect an empty container.
+          </Typography></li>
+          <li><Typography>
+            The truck-trailer proceeds to a warehouse to load goods into the container.
+          </Typography></li>
+          <li><Typography>
+            Once loaded, the truck-trailer transports the container (one or two) with goods loaded, to another warehouse or a port.
+            Upon arrival, the container is unloaded using specialized equipment.
+          </Typography></li>
+          <li><Typography>
+            If this equipment is unavailable, the trailer (with the container still on it) is detached from the truck.
+            The truck (or truck-trailer) will continue to travel for serving other requests.
+          </Typography></li>
+        </ol>
+      </Box>
     </CardContent>
     {/*<Divider/>*/}
     {/*<CardActions sx={{justifyContent: "flex-end"}}>*/}
