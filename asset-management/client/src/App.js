7fb8d2777e89c5acb825c560099cabbce3f8a15c
tienv1per <tienv1perds@gmail.com>
2024-03-08 00:26:50 +0700

    SF ui

--- asset-management/client/src/App.js
+++ asset-management/client/src/App.js
@@ -1,25 +1,21 @@
-import logo from './logo.svg';
 import './App.css';
+import {
+    BrowserRouter as Router,
+    Routes,
+    Route
+} from "react-router-dom";
+import Login from './pages/login/Login';
+import SignUp from './pages/signup/Signup';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+    return (
+        <Router>
+            <Routes>
+                <Route path='/' element={<Login/>}/>
+                <Route path='/signup' element={<SignUp/>}/>
+            </Routes>
+        </Router>
+    );
+};
 
 export default App;
