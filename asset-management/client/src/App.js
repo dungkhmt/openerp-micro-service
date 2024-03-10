ae50df6bedf49f0def9611af5d08128d656fafe2
tienv1per <tienv1perds@gmail.com>
2024-03-11 01:39:54 +0700

    fe/sidebar components

--- asset-management/client/src/App.js
+++ asset-management/client/src/App.js
@@ -1,21 +1,65 @@
-import './App.css';
+import './App.scss';
 import {
-    BrowserRouter as Router,
-    Routes,
-    Route
+    createBrowserRouter,
+    RouterProvider,
+    Outlet
 } from "react-router-dom";
 import Login from './pages/login/Login';
 import SignUp from './pages/signup/Signup';
+import Navbar from './components/navbar/Navbar';
+import Sidebar from './components/sidebar/Sidebar';
+import Home from './pages/home/Home';
+import Users from './pages/users/Users';
+import Assets from './pages/assets/Assets';
 
 function App() {
-    return (
-        <Router>
-            <Routes>
-                <Route path='/' element={<Login/>}/>
-                <Route path='/signup' element={<SignUp/>}/>
-            </Routes>
-        </Router>
-    );
+    const Layout = () => {
+        return (
+            <div className="main">
+                <Navbar/>
+                <div className="container">
+                    <div className="menuContainer">
+                        <Sidebar/>
+                    </div>
+                    <div className="contentContainer">
+                        <Outlet/>
+                    </div>
+                </div>
+            </div>
+        );
+
+    };
+
+    const router = createBrowserRouter([
+        {
+            path: "/",
+            element: <Layout />,
+            children: [
+                {
+                    path: "/",
+                    element: <Home />,
+                },
+                {
+                    path: "/users",
+                    element: <Users />,
+                },
+                {
+                    path: "/assets",
+                    element: <Assets />,
+                },
+            ],
+        },
+        {
+            path: "/login",
+            element: <Login />,
+        },
+        {
+            path: "/signup",
+            element: <SignUp />,
+        },
+    ]);
+
+    return <RouterProvider router={router} />
 };
 
 export default App;
---
+++ asset-management/client/src/pages/assets/Assets.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import "./assets.scss";
+
+const Assets = () => {
+    return (
+        <div className="assets">Assets</div>
+    );
+};
+
+export default Assets;
