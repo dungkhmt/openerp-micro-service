dfec120d1f85be0f34f8ed2316d20007795a303a
dungkhmt <dungkhmt@gmail.com>
2024-03-14 11:41:45 +0700

    init module job search platform

---
+++ job-search/openerp-oauth2-client/src/components/tab/index.js
@@ -0,0 +1,7 @@
+import { a11yProps, AntTab } from "./AntTab";
+import { AntTabs } from "./AntTabs";
+import { StyledTab } from "./StyledTab";
+import { StyledTabs } from "./StyledTabs";
+import { TabPanel } from "./TabPanel";
+
+export { AntTabs, AntTab, StyledTab, StyledTabs, TabPanel, a11yProps };
