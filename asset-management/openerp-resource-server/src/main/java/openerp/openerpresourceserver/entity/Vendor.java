4400b79ad1a1131c139350192035e1271e8f21e2
tienv1per <tienv1perds@gmail.com>
2024-03-17 17:47:31 +0700

    asset-be/vendor model entity

---
+++ asset-management/openerp-resource-server/src/main/java/openerp/openerpresourceserver/entity/Vendor.java
@@ -0,0 +1,43 @@
+package openerp.openerpresourceserver.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+
+import java.util.Date;
+
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+@Builder
+@Table(name = "asset_management_vendor")
+public class Vendor {
+    @Id
+    @Column(updatable = false, nullable = false)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    private String name;
+
+    private String phone;
+
+    private String email;
+
+    private String address;
+
+    private String image;
+
+    @Column(columnDefinition = "TEXT")
+    private  String description;
+
+    @CreatedDate
+    private Date since;
+
+    @LastModifiedDate
+    private Date last_updated;
+}
