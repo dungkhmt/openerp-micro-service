04f311822557d475d9ac3b645138254d72688bef
tienv1per <tienv1perds@gmail.com>
2024-03-21 00:51:48 +0700

    asset-be/vendor api add new

--- asset-management/openerp-resource-server/src/main/java/openerp/openerpresourceserver/controller/VendorController.java
+++ asset-management/openerp-resource-server/src/main/java/openerp/openerpresourceserver/controller/VendorController.java
@@ -6,9 +6,7 @@ import openerp.openerpresourceserver.service.VendorService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -24,5 +22,13 @@ public class VendorController {
         return ResponseEntity
             .status(HttpStatus.OK)
             .body(vendors);
+    }
+
+    @PostMapping("/add-new")
+    public ResponseEntity<?> addNewVendor(@RequestBody Vendor vendor){
+        Vendor newVendor = vendorService.addNewVendor(vendor);
+        return ResponseEntity
+            .status(HttpStatus.CREATED)
+            .body(newVendor);
     }
 }
