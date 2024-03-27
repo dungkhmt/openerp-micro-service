808b4976ab07b856ce21c99358663d99019f26a0
tienv1per <tienv1perds@gmail.com>
2024-03-28 00:26:44 +0700

    asset-be/api get vendor by id

--- asset-management/openerp-resource-server/src/main/java/openerp/openerpresourceserver/service/VendorService.java
+++ asset-management/openerp-resource-server/src/main/java/openerp/openerpresourceserver/service/VendorService.java
@@ -3,9 +3,12 @@ package openerp.openerpresourceserver.service;
 import openerp.openerpresourceserver.entity.Vendor;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface VendorService {
     List<Vendor> getAllVendors();
+
+    Optional<Vendor> getVendorById(Integer Id);
 
     Vendor addNewVendor(Vendor vendor);
 
