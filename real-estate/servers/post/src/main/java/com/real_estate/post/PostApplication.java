8dc2e717dcadfff7c0b5e3c869316b783a711aaf
vy285 <vy.lt280502@gmail.com>
2024-04-01 15:47:50 +0700

    init project

---
+++ real-estate/servers/post/src/main/java/com/real_estate/post/PostApplication.java
@@ -0,0 +1,13 @@
+package com.real_estate.post;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PostApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PostApplication.class, args);
+	}
+
+}
