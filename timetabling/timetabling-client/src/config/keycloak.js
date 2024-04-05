4feab8ab5abfed893b53b2576bef4f56e387e6ab
AnhTuan-AiT <58069686+AnhTuan-AiT@users.noreply.github.com>
2024-04-05 20:56:17 +0700

    update Keycloak config

--- timetabling/timetabling-client/src/config/keycloak.js
+++ timetabling/timetabling-client/src/config/keycloak.js
@@ -3,13 +3,12 @@ import { config } from "./constant";
 
 //
 export const initOptions = { pkceMethod: "S256" };
-export const KC_REALM = "OpenERP-Dev";
 
 // Pass initialization options as required or leave blank to load from 'keycloak.json'
 const keycloak = new Keycloak({
   url: `${config.url.KEYCLOAK_BASE_URL}`,
-  realm: KC_REALM,
-  clientId: "openerp-ui-dev",
+  realm: `${config.REALM}`,
+  clientId: `${config.CLIENT_ID}`,
 });
 
 export default keycloak;
