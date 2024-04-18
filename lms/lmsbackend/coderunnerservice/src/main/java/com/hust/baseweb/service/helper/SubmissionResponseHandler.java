bb1fde051faa6133e7140bc26dbf229b4601bb6b
NguyenDuy-Tien <tienduy2912@gmail.com>
2024-04-18 20:59:25 +0700

    update Java submission handler

--- lms/lmsbackend/coderunnerservice/src/main/java/com/hust/baseweb/service/helper/SubmissionResponseHandler.java
+++ lms/lmsbackend/coderunnerservice/src/main/java/com/hust/baseweb/service/helper/SubmissionResponseHandler.java
@@ -18,10 +18,7 @@ import org.springframework.transaction.annotation.Transactional;
 
 import java.lang.management.ManagementFactory;
 import java.lang.management.MemoryMXBean;
-import java.util.Date;
-import java.util.List;
-import java.util.Map;
-import java.util.UUID;
+import java.util.*;
 
 import static com.hust.baseweb.config.rabbitmq.ProblemContestRoutingKey.JUDGE_CUSTOM_PROBLEM;
 
@@ -33,130 +30,6 @@ public class SubmissionResponseHandler {
     private ContestSubmissionRepo contestSubmissionRepo;
     private ContestSubmissionTestCaseEntityRepo contestSubmissionTestCaseEntityRepo;
     private RabbitTemplate rabbitTemplate;
-
-    @Transactional
-    public void processSubmissionResponse(
-        List<TestCaseEntity> testCaseEntityList,
-        List<String> listSubmissionResponse,
-        ContestSubmissionEntity submission,
-        String problemEvaluationType
-    ) throws Exception {
-        int runtime = 0;
-        long score = 0;
-        int nbTestCasePass = 0;
-
-        String totalStatus;
-        String message = "";
-        boolean compileError = false;
-        boolean processing = false;
-
-        int mb = 1000 * 1000;
-        MemoryMXBean memoryBean = ManagementFactory.getMemoryMXBean();
-
-        int i = 0;
-
-        long startTime1 = System.nanoTime();
-        for (TestCaseEntity testCaseEntity : testCaseEntityList) {
-            String response = listSubmissionResponse.get(i++);
-
-            ProblemSubmission problemSubmission;
-
-            try {
-
-                problemSubmission = StringHandler.handleContestResponseSingleTestcase(
-                    response,
-                    testCaseEntity.getCorrectAnswer(),
-                    testCaseEntity.getTestCasePoint(),
-                    problemEvaluationType);
-
-                if (problemSubmission.getStatus()
-                    .equals(ContestSubmissionEntity.SUBMISSION_STATUS_COMPILE_ERROR)) {
-                    message = problemSubmission.getMessage();
-                    compileError = true;
-                    break;
-                } else if (problemSubmission
-                    .getStatus()
-                    .equals(ContestSubmissionEntity.SUBMISSION_STATUS_WAIT_FOR_CUSTOM_EVALUATION)) {
-                    processing = true;
-                }
-            } catch (Exception e) {
-                e.printStackTrace();
-                // System.out.println("LOG FOR TESTING STRING HANDLER ERROR: " + response);
-                throw new Exception("error from StringHandler");
-            }
-
-            runtime = runtime + problemSubmission.getRuntime().intValue();
-            score = score + problemSubmission.getScore();
-            nbTestCasePass += problemSubmission.getNbTestCasePass();
-
-            List<String> output = problemSubmission.getParticipantAns();
-            String participantAns = output != null && !output.isEmpty() ? output.get(0) : "";
-
-            ContestSubmissionTestCaseEntity cste = ContestSubmissionTestCaseEntity.builder()
-                .contestId(submission.getContestId())
-                .contestSubmissionId(submission.getContestSubmissionId())
-                .problemId(submission.getProblemId())
-                .testCaseId(testCaseEntity.getTestCaseId())
-                .submittedByUserLoginId(submission.getUserId())
-                .point(problemSubmission.getScore())
-                .status(StringHandler.removeNullCharacter(
-                    problemSubmission.getStatus()))
-                .participantSolutionOtput(
-                    StringHandler.removeNullCharacter(
-                        participantAns))
-                .runtime(problemSubmission.getRuntime())
-                .createdStamp(submission.getCreatedAt())
-                .build();
-
-            long startTime = System.nanoTime();
-            contestSubmissionTestCaseEntityRepo.saveAndFlush(cste);
-            long endTime = System.nanoTime();
-            log.debug(
-                "Save contestSubmissionTestCaseEntity to DB, execution time = {} ms",
-                (endTime - startTime) / 1000000);
-
-        }
-
-        long endTime1 = System.nanoTime();
-        log.debug(
-            "Total handle response time = {} ms",
-            (endTime1 - startTime1) / 1000000);
-
-        long used = memoryBean.getHeapMemoryUsage().getUsed() / mb;
-        long committed = memoryBean.getHeapMemoryUsage().getCommitted() / mb;
-        log.debug("Memory used / committed :  " + used + "mb / " + committed + "mb");
-
-        if (compileError) {
-            totalStatus = ContestSubmissionEntity.SUBMISSION_STATUS_COMPILE_ERROR;
-        } else if (processing) {
-            message = "Evaluating";
-            totalStatus = ContestSubmissionEntity.SUBMISSION_STATUS_WAIT_FOR_CUSTOM_EVALUATION;
-        } else if (nbTestCasePass == 0) {
-            totalStatus = ContestSubmissionEntity.SUBMISSION_STATUS_FAILED;
-        } else if (nbTestCasePass > 0 && nbTestCasePass < testCaseEntityList.size()) {
-            totalStatus = ContestSubmissionEntity.SUBMISSION_STATUS_PARTIAL;
-        } else {
-            message = "Successful";
-            totalStatus = ContestSubmissionEntity.SUBMISSION_STATUS_ACCEPTED;
-        }
-
-        submission.setStatus(totalStatus);
-        submission.setPoint(score);
-        submission.setTestCasePass(nbTestCasePass + " / " + testCaseEntityList.size());
-        submission.setSourceCode(submission.getSourceCode());
-        submission.setSourceCodeLanguage(submission.getSourceCodeLanguage());
-        submission.setRuntime((long) runtime);
-        submission.setMessage(message);
-        submission.setUpdateAt(new Date());
-        contestSubmissionRepo.saveAndFlush(submission);
-
-        if (processing) {
-            rabbitTemplate.convertAndSend(
-                RabbitProgrammingContestConfig.EXCHANGE,
-                JUDGE_CUSTOM_PROBLEM,
-                submission.getContestSubmissionId());
-        }
-    }
 
     @Transactional
     public void processSubmissionResponseV2(
@@ -188,12 +61,21 @@ public class SubmissionResponseHandler {
 
             try {
 
-                problemSubmission = StringHandler.handleContestResponseSingleTestcaseV2(
-                    response,
-                    testCaseEntity.getCorrectAnswer(),
-                    testCaseEntity.getTestCasePoint(),
-                    problemEvaluationType,
-                    problemTimeLimit);
+                if (Objects.equals(submission.getSourceCodeLanguage(), ContestSubmissionEntity.LANGUAGE_JAVA)) {
+                    problemSubmission = StringHandler.handleContestResponseSingleTestcase(
+                            response,
+                            testCaseEntity.getCorrectAnswer(),
+                            testCaseEntity.getTestCasePoint(),
+                            problemEvaluationType,
+                            problemTimeLimit);
+                } else {
+                    problemSubmission = StringHandler.handleContestResponseSingleTestcaseV2(
+                            response,
+                            testCaseEntity.getCorrectAnswer(),
+                            testCaseEntity.getTestCasePoint(),
+                            problemEvaluationType,
+                            problemTimeLimit);
+                }
 
                 if (problemSubmission.getStatus()
                     .equals(ContestSubmissionEntity.SUBMISSION_STATUS_COMPILE_ERROR)) {
