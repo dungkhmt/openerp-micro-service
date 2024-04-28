2ddff49816c034d7362b9b1eade89ab6d040cbd4
tienv1per <tienv1perds@gmail.com>
2024-04-28 23:49:05 +0700

    asset-be/asset api

--- asset-management/openerp-resource-server/src/main/java/openerp/openerpresourceserver/repo/AssetRepo.java
+++ asset-management/openerp-resource-server/src/main/java/openerp/openerpresourceserver/repo/AssetRepo.java
@@ -2,8 +2,16 @@ package openerp.openerpresourceserver.repo;
 
 import openerp.openerpresourceserver.entity.Asset;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface AssetRepo extends JpaRepository<Asset, Integer> {
+    @Query(value = "SELECT * FROM asset_management_asset WHERE status_id = :status_id", nativeQuery = true)
+    List<Asset> findByStatusId(Integer status_id);
+
+    @Query(value = "SELECT * FROM asset_management_asset WHERE assignee_id = :user_id", nativeQuery = true)
+    List<Asset> findByAssigneeId(String user_id);
 }
