29aee18ffc44620ee93213f763368576f5af0f4c
sucrub <bachspham@gmail.com>
2024-04-28 15:13:02 +0700

    Fix file name

--- TArecruitment/openerp-oauth2-client/src/views/AssigningScreen/index.jsx
+++ TArecruitment/openerp-oauth2-client/src/views/AssigningScreen/index.jsx
@@ -164,7 +164,10 @@ const AssigningScreen = () => {
         const url = window.URL.createObjectURL(new Blob([res.data]));
         const link = document.createElement("a");
         link.href = url;
-        link.setAttribute("download", "assign_list.xlsx");
+        link.setAttribute(
+          "download",
+          `Danh-sách-lớp-trợ-giảng-kì-${SEMESTER}.xlsx`
+        );
         document.body.appendChild(link);
         link.click();
       },
