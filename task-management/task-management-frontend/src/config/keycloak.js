bb1dee7224c712a9e975fd6ebb210a702f539384
hieugiabao <hieugiabao12@gmail.com>
2024-04-30 16:10:45 +0700

    [t-m] feat: render task to calendar

--- task-management/task-management-frontend/src/config/keycloak.js
+++ task-management/task-management-frontend/src/config/keycloak.js
@@ -2,7 +2,7 @@ import Keycloak from "keycloak-js";
 import { config } from "./config";
 
 //
-export const initOptions = { pkceMethod: "S256" };
+export const initOptions = { pkceMethod: "S256", checkLoginIframe: false };
 export const KC_REALM = config.keycloak.realm;
 
 // Pass initialization options as required or leave blank to load from 'keycloak.json'
--- task-management/task-management-frontend/src/views/project/calendar/ProjectViewCalendar.jsx
+++ task-management/task-management-frontend/src/views/project/calendar/ProjectViewCalendar.jsx
@@ -1,16 +1,39 @@
 import { Box, useMediaQuery } from "@mui/material";
-import { useState } from "react";
+import { useState, useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { useParams } from "react-router-dom";
 import CalendarWrapper from "../../../components/calendar/CalendarWrapper";
+import { fetchCalendarTasks } from "../../../store/project/calendar";
 import { AddEventSidebar } from "./AddEventSidebar";
 import Calendar from "./Calendar";
+import dayjs from "dayjs";
 
 const ProjectViewCalendar = () => {
+  const { id: projectId } = useParams();
   const mdAbove = useMediaQuery((theme) => theme.breakpoints.up("md"));
   const [addEventSidebarOpen, setAddEventSidebarOpen] = useState(false);
+  const [isInit, setIsInit] = useState(false);
+  const dispatch = useDispatch();
+  const { range, tasks } = useSelector((state) => state.calendar);
 
   const handleAddEventSidebarToggle = () => {
     setAddEventSidebarOpen(!addEventSidebarOpen);
   };
+
+  useEffect(() => {
+    // Avoid the first fetch when the component is mounted
+    if (!isInit && tasks.length > 0) {
+      setIsInit(true);
+      return;
+    }
+    dispatch(
+      fetchCalendarTasks({
+        projectId,
+        from: dayjs(range.startDate).unix(),
+        to: dayjs(range.endDate).unix(),
+      })
+    );
+  }, [dispatch, projectId, range]);
 
   return (
     <CalendarWrapper
