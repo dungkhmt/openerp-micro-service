87d7f11ff247aa4b2a134f060f450baf6f9f9f93
vandanghuy9 <vandanghuy9@gmail.com>
2024-05-22 22:12:42 +0700

    fix update jury

--- timetabling/timetabling-client/src/views/thesisdefensejuryassignment/manager/AssignTeacherAndThesisToDefenseJury.jsx
+++ timetabling/timetabling-client/src/views/thesisdefensejuryassignment/manager/AssignTeacherAndThesisToDefenseJury.jsx
@@ -36,6 +36,9 @@ function AssignTeacherAndThesisToDefenseJury() {
     if (assignedThesis?.length === 0) {
       return errorNoti("Bạn hãy lựa chọn đồ án vào hội đồng", true);
     }
+    if (assignedThesis?.length > defenseJury?.maxThesis) {
+      return errorNoti(`Hội đồng chỉ có tối đa ${defenseJury?.maxThesis} đồ án`, true);
+    }
 
     request(
       "post",
