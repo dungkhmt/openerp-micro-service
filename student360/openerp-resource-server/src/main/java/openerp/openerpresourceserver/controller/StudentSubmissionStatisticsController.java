eaaacb2f13df052b598362d620830b9d329d75c1
huuson9 <106168787+huuson9@users.noreply.github.com>
2024-05-25 18:17:03 +0700

    student360/API_decentralization

--- student360/openerp-resource-server/src/main/java/openerp/openerpresourceserver/controller/StudentSubmissionStatisticsController.java
+++ student360/openerp-resource-server/src/main/java/openerp/openerpresourceserver/controller/StudentSubmissionStatisticsController.java
@@ -7,11 +7,13 @@ import openerp.openerpresourceserver.model.StudentSubmissionDetail;
 import openerp.openerpresourceserver.service.StudentSubmissionStatisticsService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.security.Principal;
 import java.util.List;
 
 @RestController
@@ -27,20 +29,23 @@ public class StudentSubmissionStatisticsController {
         return ResponseEntity.ok().body(StudentSubmissionDetails);
     }
 
+    @PreAuthorize("hasRole('ROLE_TEACHER') or (hasRole('ROLE_STUDENT') and #id == principal.name)")
     @GetMapping("/details/{id}")
-    public ResponseEntity<?> getStatisticsDetailStudentId(@PathVariable String id) {
+    public ResponseEntity<?> getStatisticsDetailStudentId(@PathVariable String id, Principal principal) {
         StudentSubmissionDetail studentSubmissionDetail = StudentSubmissionStatisticsService.getStatisticsDetailStudentId(id);
         return ResponseEntity.ok().body(studentSubmissionDetail);
     }
 
+    @PreAuthorize("hasRole('ROLE_TEACHER') or (hasRole('ROLE_STUDENT') and #id == principal.name)")
     @GetMapping("/student-contest-statistic/{id}")
-    public ResponseEntity<?> getStaticsContestStudentId(@PathVariable String id) {
+    public ResponseEntity<?> getStaticsContestStudentId(@PathVariable String id, Principal principal) {
         StudentStatisticContest studentStatisticContest = StudentSubmissionStatisticsService.getStaticsContestStudentId(id);
         return ResponseEntity.ok().body(studentStatisticContest);
     }
 
+    @PreAuthorize("hasRole('ROLE_TEACHER') or (hasRole('ROLE_STUDENT') and #id == principal.name)")
     @GetMapping("/student-performance/{id}")
-    public ResponseEntity<?> getPerformanceStudentId(@PathVariable String id) {
+    public ResponseEntity<?> getPerformanceStudentId(@PathVariable String id, Principal principal) {
         StudentPerformance studentPerformance = StudentSubmissionStatisticsService.getPerformanceStudentId(id);
         return ResponseEntity.ok().body(studentPerformance);
     }
