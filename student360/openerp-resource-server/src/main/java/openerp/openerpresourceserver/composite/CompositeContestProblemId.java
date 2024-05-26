923ed7d38659b4c848b9a18ed409d846ea12593f
huuson9 <106168787+huuson9@users.noreply.github.com>
2024-05-26 21:50:43 +0700

    fixbug/delete_contest_contest_problem_id

---
+++ student360/openerp-resource-server/src/main/java/openerp/openerpresourceserver/composite/CompositeContestProblemId.java
@@ -0,0 +1,16 @@
+package openerp.openerpresourceserver.composite;
+
+import lombok.*;
+
+import java.io.Serializable;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+@EqualsAndHashCode
+public class CompositeContestProblemId implements Serializable {
+
+    private String contestId;
+    private String problemId;
+}
