ec00b9058a4324fe550cf2e7be5bc112b6267c6e
admin <nhl9a1vn@gmail.com>
2024-05-30 18:20:30 +0700

    feature/ add find-room screen

--- timetabling/timetabling-server/src/main/java/openerp/openerpresourceserver/generaltimetabling/model/GetEmptyRoomsRequest.java
+++ timetabling/timetabling-server/src/main/java/openerp/openerpresourceserver/generaltimetabling/model/GetEmptyRoomsRequest.java
@@ -12,8 +12,6 @@ public class GetEmptyRoomsRequest {
     private Integer weekDay;
     private Integer startTime;
     private Integer endTime;
-    private Integer maxQuantity;
-    private String priorityBuildings;
     private String crew;
     private Integer week;
 }
