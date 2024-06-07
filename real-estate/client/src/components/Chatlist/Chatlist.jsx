183b8017f30d4bb9286300a63fd25500be220c43
vy_sotatek <vy.luong@sotatek.com>
2024-06-07 16:41:18 +0700

    ui manager post, match post

---
+++ real-estate/client/src/components/Chatlist/Chatlist.jsx
@@ -0,0 +1,75 @@
+import React, { useContext, useRef, useState } from "react";
+import "./Chatlist.css";
+import { ScrollArea } from "@mantine/core";
+import { AccountContext } from "../../context/AccountContext";
+
+const Chatlist = ({ setConversationSelect, conversations }) => {
+  const [input, setInput] = useState("");
+  const { account } = useContext(AccountContext);
+
+  // eslint-disable-next-line react/prop-types
+  const filteredConversations = conversations.filter((c) =>
+    c.other.name.toLowerCase().includes(input.toLowerCase()),
+  );
+
+  let messageEnd = useRef(null);
+
+  const transferLastMessage = (lastMessage) => {
+    if (lastMessage.messageType === "TEXT") {
+      if (lastMessage.senderId === account.accountId) {
+        return "Bạn: " + lastMessage.content;
+      } else {
+        return lastMessage.content;
+      }
+    } else {
+      if (lastMessage.senderId === account.accountId) {
+        return "Bạn đã gửi một ảnh";
+      } else {
+        return "Đã gửi một ảnh";
+      }
+    }
+  };
+  return (
+    <div className="conversationList">
+      <div className="search">
+        <div className="searchBar">
+          <img src="./search.png" alt="" />
+          <input
+            type="text"
+            placeholder="Search"
+            onChange={(e) => setInput(e.target.value)}
+          />
+        </div>
+      </div>
+      <ScrollArea h={"80%"} viewportRef={messageEnd}>
+        {filteredConversations.map((coversation, index) => (
+          <div
+            className="item"
+            key={index}
+            onClick={() => setConversationSelect(coversation)}
+            style={{
+              backgroundColor: "#5183fe",
+            }}
+          >
+            <img src={coversation.other.avatar || "./avatar.png"} alt="" />
+            <div className="texts">
+              <span>{coversation.other.name}</span>
+              <p>{transferLastMessage(coversation?.messages[0])}</p>
+            </div>
+          </div>
+        ))}
+        <div
+          style={{
+            float: "left",
+            clear: "both",
+          }}
+          ref={(el) => {
+            messageEnd = el;
+          }}
+        ></div>
+      </ScrollArea>
+    </div>
+  );
+};
+
+export default Chatlist;
