27b7dd5f11c24f4860803ace2cc22192c51d6c17
Lê Anh Tuấn <anhtuan0126104@gmail.com>
2024-06-22 18:49:37 +0700

    [fix] fix overflow layout

--- lms/lmsfrontend/src/layout/Layout.jsx
+++ lms/lmsfrontend/src/layout/Layout.jsx
@@ -29,18 +29,19 @@ const Offset = styled("div")(({ theme }) => ({
 const Main = styled("main", { shouldForwardProp: (prop) => prop !== "open" })(
   ({ theme, open }) => ({
     flexGrow: 1,
+    width: "100%",
     padding: theme.spacing(3),
-    transition: theme.transitions.create("margin", {
+    transition: theme.transitions.create("padding", {
       easing: theme.transitions.easing.sharp,
       duration: theme.transitions.duration.leavingScreen,
     }),
     marginLeft: `-${drawerWidth}px`,
     ...(open && {
-      transition: theme.transitions.create("margin", {
+      transition: theme.transitions.create("padding", {
         easing: theme.transitions.easing.easeOut,
         duration: theme.transitions.duration.enteringScreen,
       }),
-      marginLeft: 0,
+      paddingLeft: `${drawerWidth + parseInt(theme.spacing(3).slice(0, -2))}px`,
     }),
   })
 );
