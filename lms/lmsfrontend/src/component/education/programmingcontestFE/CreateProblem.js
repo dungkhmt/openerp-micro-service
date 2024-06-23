e5219d7fb7dbc45c0b8a7c0630771e7397a06f99
Tien Nguyen <tienduy2912@gmail.com>
2024-06-23 22:57:10 +0700

    fix: create tag button

--- lms/lmsfrontend/src/component/education/programmingcontestFE/CreateProblem.js
+++ lms/lmsfrontend/src/component/education/programmingcontestFE/CreateProblem.js
@@ -10,7 +10,7 @@ import {
   InputAdornment,
   InputLabel,
   Link,
-  ListItemText,
+  ListItemText, ListSubheader,
   MenuItem,
   OutlinedInput,
   Select,
@@ -390,20 +390,23 @@ function CreateProblem() {
                 </Box>
               )}
             >
-              <Button
-                sx={{marginLeft: "20px"}}
-                startIcon={<AddCircleIcon/>}
-                onClick={() => setOpenModalAddNewTag(true)}
-              >
-                {t("common:addNew")}
-              </Button>
-              <ModelAddNewTag
-                isOpen={openModalAddNewTag}
-                handleSuccess={() => {
-                  getAllTags(handleGetTagsSuccess)
-                }}
-                handleClose={() => setOpenModalAddNewTag(false)}
-              />
+              <ListSubheader>
+                <Button
+                  sx={{marginLeft: "20px"}}
+                  startIcon={<AddCircleIcon/>}
+                  onClick={() => setOpenModalAddNewTag(true)}
+                >
+                  {t("common:addNew")}
+                </Button>
+                <ModelAddNewTag
+                  isOpen={openModalAddNewTag}
+                  handleSuccess={() => {
+                    getAllTags(handleGetTagsSuccess)
+                  }}
+                  handleClose={() => setOpenModalAddNewTag(false)}
+                />
+              </ListSubheader>
+
               {tags.map((tag) => (
                 <MenuItem key={tag.tagId} value={tag}>
                   <Checkbox checked={selectedTags.indexOf(tag) > -1}/>
