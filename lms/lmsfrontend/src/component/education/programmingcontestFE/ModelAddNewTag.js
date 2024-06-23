281d05c25f189625c125a3e4e53d6cdfe8e597c9
Tien Nguyen <tienduy2912@gmail.com>
2024-06-23 23:49:31 +0700

    fix: create tag button

--- lms/lmsfrontend/src/component/education/programmingcontestFE/ModelAddNewTag.js
+++ lms/lmsfrontend/src/component/education/programmingcontestFE/ModelAddNewTag.js
@@ -45,6 +45,7 @@ const ModelAddNewTag = ({isOpen, handleSuccess, handleClose}) => {
       <TextField
         fullWidth
         required
+        id="tag-name"
         label={"Tag"}
         placeholder="Tag"
         defaultValue={tagName}
@@ -55,6 +56,7 @@ const ModelAddNewTag = ({isOpen, handleSuccess, handleClose}) => {
       />
       <TextField
         fullWidth
+        id="tag-description"
         label={t("common:description")}
         placeholder={t("common:description")}
         value={tagDescription}
