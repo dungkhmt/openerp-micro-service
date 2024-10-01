fcad946a1db8c31dfd187806746f53c03280ca2c
dungkhmt <dungkhmt@gmail.com>
2024-10-01 23:24:55 +0700

    add create log function

--- lmsanalytic/openerp-resource-server/src/main/java/openerp/openerpresourceserver/log/controller/LogController.java
+++ lmsanalytic/openerp-resource-server/src/main/java/openerp/openerpresourceserver/log/controller/LogController.java
@@ -1,6 +1,7 @@
 package openerp.openerpresourceserver.log.controller;
 
 import lombok.AllArgsConstructor;
+import lombok.extern.log4j.Log4j2;
 import openerp.openerpresourceserver.log.entity.LmsLog;
 import openerp.openerpresourceserver.log.model.LmsLogModelCreate;
 import openerp.openerpresourceserver.log.service.LmsLogService;
@@ -12,6 +13,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 import java.security.Principal;
 
+@Log4j2
 @RestController
 @AllArgsConstructor(onConstructor_ = @Autowired)
 public class LogController {
@@ -20,7 +22,7 @@ public class LogController {
     @PostMapping("/log/create-log")
     public ResponseEntity<?> createLog(Principal principal, @RequestBody LmsLogModelCreate I){
         LmsLog alog = lmsLogService.save(I);
-        
+        log.info("create a log");
         return ResponseEntity.ok().body(alog);
     }
 }
