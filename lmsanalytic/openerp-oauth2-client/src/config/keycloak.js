b4af4667aa7322ae5dec64c31552b4d3b653e8d6
hieugiabao <95536646+hieugiabao@users.noreply.github.com>
2024-10-04 08:42:04 +0700

    lms-analytic change keycloak client id

--- lmsanalytic/openerp-oauth2-client/src/config/keycloak.js
+++ lmsanalytic/openerp-oauth2-client/src/config/keycloak.js
@@ -9,7 +9,7 @@ export const KC_REALM = "OpenERP-Dev";
 const keycloak = new Keycloak({
   url: `${config.url.KEYCLOAK_BASE_URL}`,
   realm: KC_REALM,
-  clientId: "openerp-ui-dev",
+  clientId: process.env.NODE_ENV === "development" ? "openerp-ui-dev" : "lms-analytic-ui",
 });
 
 export default keycloak;
