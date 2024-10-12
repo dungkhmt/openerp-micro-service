167bbc28d13483b479936aff002d57fc319c855e
Duong Hoang Hai <hoanghai2003vp@gmail.com>
2024-10-12 19:04:18 +0700

    Initiate project

---
+++ wms_haidh/wms-frontend/src/config/menuconfig/demo.js
@@ -0,0 +1,14 @@
+export const demo = {
+  id: "MENU_DEMO",
+  icon: "DashboardIcon",
+  text: "Demo1",
+  child: [
+    {
+      id: "MENU_DEMO.DEMO",
+      path: "/demo",
+      isPublic: true,
+      text: "Demo2",
+      child: [],
+    },
+  ],
+};
