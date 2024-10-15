6a788c58c470e035bd846edfdb0a045e98939015
Duong Hoang Hai <hoanghai2003vp@gmail.com>
2024-10-15 17:56:17 +0700

    Modify UI

--- wms_haidh/wms-frontend/src/config/menuconfig/demo.js
+++ wms_haidh/wms-frontend/src/config/menuconfig/demo.js
@@ -1,14 +1,14 @@
 export const demo = {
   id: "MENU_DEMO",
   icon: "DashboardIcon",
-  text: "Demo1",
+  text: "Product management",
   child: [
     {
       id: "MENU_DEMO.DEMO",
       path: "/demo",
       isPublic: true,
-      text: "Demo2",
+      text: "Product management",
       child: [],
-    },
+    }
   ],
 };
--- wms_haidh/wms-frontend/src/views/DemoScreen.jsx
+++ wms_haidh/wms-frontend/src/views/DemoScreen.jsx
@@ -16,12 +16,12 @@ import {
   User,
   Pagination,
 } from "@nextui-org/react";
-import {PlusIcon} from "./PlusIcon";
-import {VerticalDotsIcon} from "./VerticalDotsIcon";
-import {SearchIcon} from "./SearchIcon";
+import { PlusIcon } from "./PlusIcon";
+import { VerticalDotsIcon } from "./VerticalDotsIcon";
+import { SearchIcon } from "./SearchIcon";
 import { ChevronDownIcon } from "./ChvronDownIcon";
-import {columns, users, statusOptions} from "./data";
-import {capitalize} from "./utils";
+import { columns, users, statusOptions } from "./data";
+import { capitalize } from "./utils";
 
 const statusColorMap = {
   active: "success",
@@ -42,6 +42,8 @@ export default function App() {
     direction: "ascending",
   });
   const [page, setPage] = React.useState(1);
+
+  const pages = Math.ceil(users.length / rowsPerPage);
 
   const hasSearchFilter = Boolean(filterValue);
 
@@ -68,8 +70,6 @@ export default function App() {
     return filteredUsers;
   }, [users, filterValue, statusFilter]);
 
-  const pages = Math.ceil(filteredItems.length / rowsPerPage);
-
   const items = React.useMemo(() => {
     const start = (page - 1) * rowsPerPage;
     const end = start + rowsPerPage;
@@ -94,7 +94,10 @@ export default function App() {
       case "name":
         return (
           <User
-            avatarProps={{radius: "lg", src: user.avatar}}
+            avatarProps={{ radius: "full", size: "sm", src: user.avatar }}
+            classNames={{
+              description: "text-default-500",
+            }}
             description={user.email}
             name={cellValue}
           >
@@ -105,22 +108,27 @@ export default function App() {
         return (
           <div className="flex flex-col">
             <p className="text-bold text-small capitalize">{cellValue}</p>
-            <p className="text-bold text-tiny capitalize text-default-400">{user.team}</p>
+            <p className="text-bold text-tiny capitalize text-default-500">{user.team}</p>
           </div>
         );
       case "status":
         return (
-          <Chip className="capitalize" color={statusColorMap[user.status]} size="sm" variant="flat">
+          <Chip
+            className="capitalize border-none gap-1 text-default-600"
+            color={statusColorMap[user.status]}
+            size="sm"
+            variant="dot"
+          >
             {cellValue}
           </Chip>
         );
       case "actions":
         return (
           <div className="relative flex justify-end items-center gap-2">
-            <Dropdown>
+            <Dropdown className="bg-background border-1 border-default-200">
               <DropdownTrigger>
-                <Button isIconOnly size="sm" variant="light">
-                  <VerticalDotsIcon className="text-default-300" />
+                <Button isIconOnly radius="full" size="sm" variant="light">
+                  <VerticalDotsIcon className="text-default-400" />
                 </Button>
               </DropdownTrigger>
               <DropdownMenu>
@@ -136,22 +144,11 @@ export default function App() {
     }
   }, []);
 
-  const onNextPage = React.useCallback(() => {
-    if (page < pages) {
-      setPage(page + 1);
-    }
-  }, [page, pages]);
-
-  const onPreviousPage = React.useCallback(() => {
-    if (page > 1) {
-      setPage(page - 1);
-    }
-  }, [page]);
-
   const onRowsPerPageChange = React.useCallback((e) => {
     setRowsPerPage(Number(e.target.value));
     setPage(1);
   }, []);
+
 
   const onSearchChange = React.useCallback((value) => {
     if (value) {
@@ -161,11 +158,6 @@ export default function App() {
       setFilterValue("");
     }
   }, []);
-
-  const onClear = React.useCallback(()=>{
-    setFilterValue("")
-    setPage(1)
-  },[])
 
   const topContent = React.useMemo(() => {
     return (
@@ -183,7 +175,11 @@ export default function App() {
           <div className="flex gap-3">
             <Dropdown>
               <DropdownTrigger className="hidden sm:flex">
-                <Button endContent={<ChevronDownIcon className="text-small" />} variant="flat">
+                <Button
+                  endContent={<ChevronDownIcon className="text-small" />}
+                  size="md"
+                  variant="flat"
+                >
                   Status
                 </Button>
               </DropdownTrigger>
@@ -204,7 +200,11 @@ export default function App() {
             </Dropdown>
             <Dropdown>
               <DropdownTrigger className="hidden sm:flex">
-                <Button endContent={<ChevronDownIcon className="text-small" />} variant="flat">
+                <Button
+                  endContent={<ChevronDownIcon className="text-small" />}
+                  size="md"
+                  variant="flat"
+                >
                   Columns
                 </Button>
               </DropdownTrigger>
@@ -223,8 +223,12 @@ export default function App() {
                 ))}
               </DropdownMenu>
             </Dropdown>
-            <Button color="primary" endContent={<PlusIcon />}>
-              Add New
+            <Button
+              className="bg-foreground text-background"
+              endContent={<PlusIcon />}
+              size="md"
+            >
+              Add Product
             </Button>
           </div>
         </div>
@@ -248,50 +252,68 @@ export default function App() {
     filterValue,
     statusFilter,
     visibleColumns,
+    onSearchChange,
     onRowsPerPageChange,
     users.length,
-    onSearchChange,
     hasSearchFilter,
   ]);
 
   const bottomContent = React.useMemo(() => {
     return (
       <div className="py-2 px-2 flex justify-between items-center">
-        <span className="w-[30%] text-small text-default-400">
-          {selectedKeys === "all"
-            ? "All items selected"
-            : `${selectedKeys.size} of ${filteredItems.length} selected`}
-        </span>
         <Pagination
-          isCompact
           showControls
-          showShadow
-          color="primary"
+          classNames={{
+            cursor: "bg-foreground text-background",
+          }}
+          color="default"
+          isDisabled={hasSearchFilter}
           page={page}
           total={pages}
+          variant="light"
           onChange={setPage}
         />
-        <div className="hidden sm:flex w-[30%] justify-end gap-2">
-          <Button isDisabled={pages === 1} size="sm" variant="flat" onPress={onPreviousPage}>
-            Previous
-          </Button>
-          <Button isDisabled={pages === 1} size="sm" variant="flat" onPress={onNextPage}>
-            Next
-          </Button>
-        </div>
+        <span className="text-small text-default-400">
+          {selectedKeys === "all"
+            ? "All items selected"
+            : `${selectedKeys.size} of ${items.length} selected`}
+        </span>
       </div>
     );
   }, [selectedKeys, items.length, page, pages, hasSearchFilter]);
 
+  const classNames = React.useMemo(
+    () => ({
+      wrapper: ["max-h-[382px]", "max-w-3xl"],
+      th: ["bg-transparent", "text-default-500", "border-b", "border-divider"],
+      td: [
+        // changing the rows border radius
+        // first
+        "group-data-[first=true]:first:before:rounded-none",
+        "group-data-[first=true]:last:before:rounded-none",
+        // middle
+        "group-data-[middle=true]:before:rounded-none",
+        // last
+        "group-data-[last=true]:first:before:rounded-none",
+        "group-data-[last=true]:last:before:rounded-none",
+      ],
+    }),
+    [],
+  );
+
   return (
     <Table
+      isCompact
+      removeWrapper
       aria-label="Example table with custom cells, pagination and sorting"
-      isHeaderSticky
       bottomContent={bottomContent}
       bottomContentPlacement="outside"
-      classNames={{
-        wrapper: "max-h-[382px]",
+      checkboxesProps={{
+        classNames: {
+          wrapper: "after:bg-foreground after:text-background text-background",
+        },
       }}
+      classNames={classNames}
       selectedKeys={selectedKeys}
       selectionMode="multiple"
       sortDescriptor={sortDescriptor}
