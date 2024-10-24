682536e669224af9f2eff7cacfd83d4a92cace99
dungkhmt <dungkhmt@gmail.com>
2024-10-24 22:45:34 +0700

    remove dashboard

--- lmsanalytic/openerp-oauth2-client/src/components/lmslog/logs.js
+++ lmsanalytic/openerp-oauth2-client/src/components/lmslog/logs.js
@@ -6,7 +6,7 @@ import EditIcon from "@mui/icons-material/Edit";
 import DeleteIcon from "@mui/icons-material/Delete";
 import { toFormattedDateTime } from "../../utils/dateutils";
 import UserFeatures from "./userfeatures";
-import DashBoard from "./dashboard";
+
 function LmsLogs() {
 
     const [logs, setLogs] = useState([]);
