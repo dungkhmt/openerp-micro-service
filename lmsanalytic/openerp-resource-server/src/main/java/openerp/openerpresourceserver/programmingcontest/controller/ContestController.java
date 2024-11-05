14b9e43d67235b1d192a70a9363488d0bf9bf660
dungkhmt <dungkhmt@gmail.com>
2024-11-05 22:05:48 +0700

    add external api get contest submission

--- lmsanalytic/openerp-resource-server/src/main/java/openerp/openerpresourceserver/programmingcontest/controller/ContestController.java
+++ lmsanalytic/openerp-resource-server/src/main/java/openerp/openerpresourceserver/programmingcontest/controller/ContestController.java
@@ -2,6 +2,7 @@ package openerp.openerpresourceserver.programmingcontest.controller;
 
 import lombok.AllArgsConstructor;
 import lombok.extern.log4j.Log4j2;
+import openerp.openerpresourceserver.callexternalapi.service.ApiService;
 import openerp.openerpresourceserver.programmingcontest.entity.ProgrammingContestProblemRanking;
 import openerp.openerpresourceserver.programmingcontest.entity.ProgrammingContestRanking;
 import openerp.openerpresourceserver.programmingcontest.model.ContestModelRepsonse;
@@ -25,6 +26,8 @@ public class ContestController {
     private LmsContestSubmissionService lmsContestSubmissionService;
     private ProgrammingContestProblemRankingService programmingContestProblemRankingService;
     private ProgrammingContestRankingService programmingContestRankingService;
+
+    private ApiService apiService;
 
     @PostMapping("/create-contest-submission")
     public ResponseEntity<?> createContestSubmission(Principal principal, @RequestBody ModelCreateContestSubmission m){
@@ -59,6 +62,12 @@ public class ContestController {
     public ResponseEntity<?> getContestIds(Principal principal){
         List<ContestModelRepsonse> res = programmingContestRankingService.getContestAllIds();
         return ResponseEntity.ok().body(res);
+    }
+
+    @GetMapping("/synchronize-contest-submission")
+    public ResponseEntity<?> synchronizeContestSubmission(Principal principal){
+        apiService.callLogAPI("https://hustack.soict.ai/get-contest-submissions/",null);
+        return ResponseEntity.ok().body("OK");
     }
 }
 
