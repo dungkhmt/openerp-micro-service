324a639728d735a82685b58a749e7ad41f677d5e
dungkhmt <dungkhmt@gmail.com>
2024-11-20 14:59:41 +0700

    for testing

--- lmsanalytic/openerp-resource-server/src/main/java/openerp/openerpresourceserver/scheduledservice/LmsContestSubmissionProcessor.java
+++ lmsanalytic/openerp-resource-server/src/main/java/openerp/openerpresourceserver/scheduledservice/LmsContestSubmissionProcessor.java
@@ -172,7 +172,8 @@ public class LmsContestSubmissionProcessor {
     @Transactional
     public void processMigrateContestSubmissions(){
         Date currentDate = new Date();
-        //log.info("processMigrateContestSubmissions, run at time point {}",currentDate);
+        log.info("processMigrateContestSubmissions, run at time point {} and RETURN",currentDate);
+        if(true) return;
         ModelInputGetContestSubmissionPage m = new ModelInputGetContestSubmissionPage();
         List<LmsanalyticSystemParams> params = lmsanalyticSystemParamsRepo.findAllByParam(LmsanalyticSystemParams.MIGRATE_CONTEST_SUBMISSION_NUMBER_ITEMS_PER_QUERY);
         int length = 20;
